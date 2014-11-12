3f8482ff3c0dd0d4db89734186f58fc74db0004f
Paul Hyman <mail@phyman.com>
2014-11-11 19:03:07 -0600

    First 3 are done

--- scripts.js
+++ scripts.js
@@ -1,32 +1,67 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
-    // your code here
-  },
-  
-  reversed: function(){  
-    // your code here
+
+  largestEl: function(arr)
+  {
+    var largestElement = null;
+    for (var i = arr.length - 1; i >= 0; i--)
+    {
+      if ( arr[i] > largestElement )
+      {
+        largestElement = arr[i];
+      }
+    }
+    return largestElement;
   },
 
-  loudSnakeCase: function(){  
-    // your code here
+//-------------
+
+  reversed: function(str){
+    return str.split('').reverse().join('');
   },
 
-  compareArrays: function(){ 
+//-------------
+
+  loudSnakeCase: function(str){
+    console.log(str);
+    // unicode punct blocks in regular expression
+     var punctRE = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#\$%&\(\)\*\+,\-\.\/:;<=>\?@\[\]\^_`\{\|\}~]/g;
+    // var spaceRE = /\s+/g;
+
+    var nstr = str.split(' ');
+
+    // TODO - fix this super ghetto lazy search and removal of an empty string
+    var n = nstr.indexOf("");
+    if (n) {nstr.splice(n,1);}
+    console.log(nstr);
+
+    for (var i = 0; i < nstr.length; i++) {
+      nstr[i] = (nstr[i].charAt(0).toUpperCase() + nstr[i].substr(1).toLowerCase()).replace(punctRE, '');
+    }
+
+    str = nstr.join('_').toString();
+    return str;
+  },
+
+//-------------
+
+  compareArrays: function(){
     // your code here (replace the return)
-    return "Finish compareArrays first!" 
+    return "Finish compareArrays first!"
   },
 
-  fizzBuzz: function(){  
+//-------------
+
+  fizzBuzz: function(index_limit){
     // your code here
   },
 
-  myMap: function(){  
+  myMap: function(arr, func){
     // your code here
   },
 
-  primes: function(){  
+  primes: function(prime_ceil){
     // your code here
   },
-}
+};
