b5642a948a7f25a81027d004ec912d7a60c6c3e3
Dan Berger <dsberger@gmail.com>
2015-04-14 16:22:19 -0700

    persistent is_prime bug

--- scripts.js
+++ scripts.js
@@ -61,7 +61,25 @@ var sprintFunctions = {
     return output;
   },
 
-  primes: function(){  
-    // your code here
+  primes: function(max){
+    var output = [];
+    for(i = 1; i < max; i++){
+      if(is_prime(i)){
+        output.push(i);
+      };
+    };
+    return output;
   },
+
+  is_prime: function(num){
+    if( num == 1) {
+      return false;
+    }
+    for(i = 2; i < num; i++){
+      if( num%i == 0 ){
+	return false;
+      };
+    };
+    return true;
+  }
 }
