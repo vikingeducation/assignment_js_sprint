85ba6c18f5d593d0001ea9f20b382b5be5789e80
Dan Berger <dsberger@gmail.com>
2015-04-14 15:28:21 -0700

    fizzBuzz working

--- scripts.js
+++ scripts.js
@@ -37,8 +37,20 @@ var sprintFunctions = {
     return matched;
   },
 
-  fizzBuzz: function(){  
-    // your code here
+  fizzBuzz: function(num){  
+    var output = [];
+    for( var i = 1; i <= num; i++){
+      if( i%15 == 0 ){
+	output.push("FIZZBUZZ");
+      } else if( i%3 == 0 ){
+	output.push("FIZZ");
+      } else if( i%5 == 0 ){
+	output.push("BUZZ");
+      } else { 
+	output.push(i);
+      };
+    };
+    return output;
   },
 
   myMap: function(){  
