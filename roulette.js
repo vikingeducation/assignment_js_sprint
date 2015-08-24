4f6a0d1e833db56a8dc991db97706015cf142bbb
ayva <olga.ayva@gmail.com>
2015-08-24 14:39:38 -0700

    roulette proceed

--- roulette.js
+++ roulette.js
@@ -3,11 +3,14 @@ function Roulette(starting_money){
 	this.bankroll = function(){
 		return "You now have $" + this.money.toString() + "."
 	};
-	this.spin = function(bet, square){
-		var result = Math.ceil(Math.random() * 36);
-		if (result === square) {
-			this.money += bet * 35;
-			console.log("You win $" + (bet * 35).toString() + "!!!");
+	this.spin = function(bet, squares){
+		var result = Math.floor(Math.random() * 37);
+		//squares is an array
+
+		if (squares.includes(result)) {
+			mult = 36/(squares.length)-1
+			this.money += bet * mult;
+			console.log("You win $" + (bet * mult).toString() + "!!!");
 			console.log(this.bankroll());
 		} else {
 			this.money -= bet;
@@ -15,12 +18,15 @@ function Roulette(starting_money){
 			console.log(this.bankroll());
 		}
 	};
+	
 	this.buyIn = function(buyin){
 		this.money += buyin;
 		console.log("You bought in $" + buyin.toString() + ".");
 		console.log(this.bankroll());
 	};
+
+
 	this.oneToNinteenSpin = function(bet){
-		this.spin(bet, )
+		this.spin(bet)
 	}
 }
