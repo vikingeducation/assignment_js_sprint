bb92dae7375a570bea66dcdef5e398f7d1f182e0
Garrett <garrettweinert@gmail.com>
2015-08-24 16:54:57 -0700

    finished roulette

--- roulette.js
+++ roulette.js
@@ -1,62 +1,121 @@
-// r = new Roulette( 100 )  // starting bankroll $100
-// r.spin( 10, 24 )         // bet 10 on 24
-// // You Win $350, the spin was 24!!!
-// // You now have $440.
-// r.spin( 50, 13 )
-// // You Lose, the spin was 11 :(
-// // You now have $390
-// r.bankroll()
-// // You now have $390
-// r.buyIn( 1000 )
-// // You bought in $1000
-// // You now have $1390
-
-// Roulette obj with method: spin, bankroll, buyIn
 
 function Roulette(starting_amt){
+  
   this.spin = function(amt, guess){
     //random number
-    console.log("You bet $"+ amt+ "on "+ guess);
-    // var result = Math.floor(Math.random() * 37);
-    var result = 37;
-    if (result===37){
-      result = "00";
+    if( canPlay(amt) ){
+
+      console.log("You bet $"+ amt+ "on "+ guess);
+      var result = Math.floor(Math.random() * 37);
+      
+      if (result===37){
+        result = "00";
+      }
+      console.log("Spun number: " + result);
+
+      guessChecker(guess, amt, result);
     }
-    console.log("Spun number: " + result);
+  };
+
+  var guessChecker = function(guess, amt, result){
     //check against guess
     switch(guess){
       case(result):
-        this.bankroll += amt*35;
-        console.log("You won $" + amt*35);
+        win(amt, 35);
         break;
       case("00"):
-        this.bankroll += amt*35;
-        console.log("You won $" + amt*35);
+        win(amt, 35);
         break;
       case("even"):
-        if (result%2 === 0 && guess !== "00"){
-          this.bankroll += amt;
-          console.log("You won $" + amt);
+        if (result !== "00" && result%2 === 0 ){
+          win(amt, 1);
         }else {
-          this.bankroll -= amt;
-          console.log ("You lost $" + amt);
+          loss(amt);
         }
         break;
       case("odd"):
         if (result%2 !== 0){
-          this.bankroll += amt;
-          console.log("You won $" + amt);
+          win(amt, 1);
         }else {
-          this.bankroll -= amt;
-          console.log ("You lost $" + amt);
+          loss(amt);
         }
         break;
+      case("1 to 18"):
+        if (result >= 1 && result <= 18){
+          win(amt, 1);
+        } else{
+          loss(amt);
+        }
+        break;
+      
+      case("19 to 36"):
+        if (result >= 19 && result <= 36){
+          win(amt, 1);
+        } else{
+          loss(amt);
+        }
+        break;
+      
+      case("1st 12"):
+        if (result <= 12 && result >= 1){
+          win(amt, 2);
+        } else{
+          loss(amt);
+        }
+        break;
+      
+      case("2nd 12"):
+        if (result <= 24 && result >= 13){
+          win(amt, 2);
+        } else{
+          loss(amt);
+        }
+        break;
+      
+      case("3rd 12"):
+        if (result <= 36 && result >= 25){
+          win(amt, 2);
+        } else{
+          loss(amt);
+        }
+        break;
+
       default:
-        this.bankroll -= amt;
-        console.log ("You lost $" + amt);
+        loss(amt);
     }
   };
-  this.bankroll = starting_amt;
+
+  var canPlay = function(amt){
+    if( amt < 1 ){
+      console.log("Please make a positive bet");
+      return false;
+    }
+    else if( piggyBank <= 0 || piggyBank - amt < 0 ){
+      console.log("Insufficient funds");
+      return false;
+    }
+    else{
+      return true;
+    }
+  };
+
+  var win = function(amt, odds){
+    piggyBank += amt*odds;
+    console.log("You won $" + amt*odds);
+  };
+
+  var loss = function(amt){
+    piggyBank -= amt;
+    console.log ("You lost $" + amt);
+
+  };
+
+  var piggyBank = starting_amt;
+
+  this.bankroll = function(){
+    return piggyBank;
+  };
+  
   this.buyIn = function(amt){
     this.bankroll += amt;
   };
