237bdde19f0ae7c7d672395d27eeb5b069415645
Joseph Lozano <lozanojoseph@gmail.com>
2015-08-24 10:48:17 -0700

    finish funcs

--- scripts.js
+++ scripts.js
@@ -55,15 +55,47 @@ var sprintFunctions = {
     return result;
   },
 
-  fizzBuzz: function(){
-    // your code here
+  fizzBuzz: function(num){
+    var arr = [];
+    for (var i = 1; i <= num; i++){
+      if (i % 15 == 0) {
+        arr.push("FIZZBUZZ");
+      }
+      else if (i % 3 == 0){
+        arr.push("FIZZ");
+      }
+      else if (i % 5 == 0){
+        arr.push("BUZZ");
+      }
+      else {
+        arr.push(i);
+      };
+    };
+    return arr;
   },
 
-  myMap: function(){
-    // your code here
+  myMap: function(arr, func){
+    results = [];
+    for (var i = 0; i < arr.length; i++) {
+      results.push(func(arr[i]));
+    };
+    return results;
   },
 
-  primes: function(){
-    // your code here
+  primes: function(num){
+    var results = [];
+    var prime;
+    for (var i = 2; i < num; i++){
+      prime = true;
+      for (var j = i-1; j > 1; j--){
+        if (i % j == 0){
+          prime = false;
+        };
+      };
+      if (prime){
+        results.push(i);
+      };
+    };
+    return results;
   },
 }
