25e2da8fa5d53f28aa8a73271a2f4a37253419ec
Alice Fung <alice.o.fung@gmail.com>
2015-08-24 12:57:17 -0400

    up to loudSnakeCase

--- scripts.js
+++ scripts.js
@@ -1,32 +1,54 @@
+"use strict";
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
+  largestEl: function(array){
     // your code here
-  },
-  
-  reversed: function(){  
-    // your code here
+    var maxValue = array[0];
+    for(var i=0; i < array.length; i++){
+      if(array[i] > maxValue){
+        maxValue = array[i];
+      }
+    }
+    return maxValue;
   },
 
-  loudSnakeCase: function(){  
+  reversed: function(string){
     // your code here
+    var reversedString = "";
+    for(var i=string.length-1; i > -1; i--){
+      reversedString+= string[i];
+    }
+    return reversedString;
   },
 
-  compareArrays: function(){ 
+  loudSnakeCase: function(string){
+    // your code here
+    var loud = [];
+    string = string.replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    string = string.replace(/\s+/g, " ");
+    var string_array = string.split(" ");
+    for(var i=0; i< string_array.length; i++){
+      loud[i] = string_array[i][0].toUpperCase() + string_array[i].slice(1,string_array[i].length);
+    }
+    loud = loud.join("_");
+    return loud;
+  },
+
+  compareArrays: function(){
     // your code here (replace the return)
-    return "Finish compareArrays first!" 
+    return "Finish compareArrays first!"
   },
 
-  fizzBuzz: function(){  
+  fizzBuzz: function(){
     // your code here
   },
 
-  myMap: function(){  
+  myMap: function(){
     // your code here
   },
 
-  primes: function(){  
+  primes: function(){
     // your code here
   },
 }
