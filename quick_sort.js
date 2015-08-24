dee704a5807fbf011944ab59c205e212db23ea2a
alokp <alokpra@gmail.com>
2015-08-24 14:35:15 -0700

    working on quick sort

--- quick_sort.js
+++ quick_sort.js
@@ -1,9 +1,28 @@
-var quickSort = function(arr) {
+var quickSort = function qSort (arr) {
+  var result = []
+  var pivot_index = Math.floor((Math.random()*arr.length));
+  var pivot_value = arr[pivot_index];
+  var left_arr = [];
+  var right_arr = [];
 
-  var pivot =
+  console.log(pivot_value);
 
+  if (arr.length <= 1) {
+    return arr;
+  };
+
+  for(var i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+    if (arr[i] < pivot_value) {
+      left_arr.push(arr[i]);
+    } else {
+      right_arr.push(arr[i]);
+    };
+  };
+
+  result = qSort(left_arr).concat(qSort(right_arr));
+  return result;
 };
-
 
 // Pick an element, called a pivot, from the array.
 // Reorder the array so that all elements with values less than the pivot come before the pivot, while all elements with values greater than the pivot come after it (equal values can go either way). After this partitioning, the pivot is in its final position. This is called the partition operation.
