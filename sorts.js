cf4d7984dcec413c463edc535a9b066fbc11f278
mikebutts <mgbutts@gmail.com>
2015-08-25 18:26:07 -0400

    working on recursive3 js

--- sorts.js
+++ sorts.js
@@ -46,6 +46,79 @@ function merge (array1, array2) {
   return sorted_array;
 }
 
-console.log( merge_sort([6,5,4,3,2,1]) );
-console.log( merge_sort([1,7,3,2,9,0,5]) );
-console.log( merge_sort([1,2,3,4,5,6,7,8]) );
+//console.log( merge_sort([6,5,4,3,2,1]) );
+//console.log( merge_sort([1,7,3,2,9,0,5]) );
+//console.log( merge_sort([1,2,3,4,5,6,7,8]) );
+
+
+
+ 
+function quick_sort(arr)
+{
+    var stack = [arr];
+    var sorted = [];
+ 
+    while (stack.length > 0) {
+ 
+        var temp = stack.pop(), 
+            temp_length = temp.length;
+ 
+        if (temp_length == 1) {
+            sorted.push(temp[0]);
+            continue;
+        }
+        var pivot = temp[0];
+        var left = [], 
+            right = [];
+ 
+        for (var i = 1; i < temp_length; i++) {
+            if (temp[i] < pivot) {
+                left.push(temp[i]);
+            } else {
+                right.push(temp[i]);
+            }
+        }
+ 
+        left.push(pivot);
+ 
+        if (right.length > 0)
+            stack.push(right);
+        if (left.length > 0)
+            stack.push(left);
+ 
+    }
+ 
+    console.log(sorted);
+}
+
+
+ 
+function quicksort(arr)
+{
+    if ((arr.length == 0) ||  (arr.length === 1))
+        return [];
+ 
+    var left = new Array();
+    var right = new Array();
+    var pivot = arr[Math.floor(Math.random() * (arr.length))];
+ 
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+           left.push(arr[i]);
+        } else {
+           right.push(arr[i]);
+        }
+    }
+ 
+    return quicksort(left).concat(pivot, quicksort(right));
+}
+ 
+
+console.log( quick_sort([6,5,4,3,2,1]) );
+console.log( quick_sort([1,7,3,2,9,0,5]) );
+console.log( quick_sort([1,2,3,4,5,6,7,8]) );
+
+
+console.log( quicksort([6,5,4,3,2,1]) );
+console.log( quicksort([1,7,3,2,9,0,5]) );
+console.log( quicksort([1,2,3,4,5,6,7,8]) );
