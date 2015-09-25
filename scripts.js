d4d1eb9be511cb7fa1df3297355d8ad7822a4d80
ramusbucket <ramu.dup.11@gmail.com>
2015-09-25 23:10:33 +0530

    Optimized fizzbuzz

--- scripts.js
+++ scripts.js
@@ -37,7 +37,7 @@ var sprintFunctions = {
   fizzBuzz: function(number){
     result = [];
     for (var i = 1; i <= number; i++) {
-      if (i % 5 == 0 && i % 3 == 0) {
+      if (i % 15 == 0) {
         result.push("FIZZBUZZ")
       }
       else if (i % 3 == 0) {
