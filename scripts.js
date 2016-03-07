45f4c641fa430cb0d14a847067cc2fc6cd8b7813
Andrew Baik <and.baik@gmail.com>
2016-03-07 11:46:09 -0500

    largest element function done

--- scripts.js
+++ scripts.js
@@ -1,32 +1,39 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
-    // your code here
+  largestEl: function(input_array){
+    var max = 0;
+    var array_length = input_array.length;
+    for(var i = 0; i < array_length; i++) {
+      if (input_array[i] > max) {
+        max = input_array[i];
+      }
+    }
+    return max;
   },
-  
-  reversed: function(){  
+
+  reversed: function(){
     // your code here
   },
 
-  loudSnakeCase: function(){  
+  loudSnakeCase: function(){
     // your code here
   },
 
-  compareArrays: function(){ 
+  compareArrays: function(){
     // your code here (replace the return)
-    return "Finish compareArrays first!" 
+    return "Finish compareArrays first!"
   },
 
-  fizzBuzz: function(){  
+  fizzBuzz: function(){
     // your code here
   },
 
-  myMap: function(){  
+  myMap: function(){
     // your code here
   },
 
-  primes: function(){  
+  primes: function(){
     // your code here
   },
 }
