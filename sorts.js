b4e59470e4984a51a25e545aad45a2950c867aea
Joshua Masland-Sarani <j.masland@gmail.com>
2016-03-07 13:48:48 -0500

    mergesort

---
+++ sorts.js
@@ -0,0 +1,55 @@
+function bubbleSort( array ) {
+  var swaps;
+  while ( swaps !== 0 ) {
+    swaps = 0;
+    for ( var i = 0; i < array.length - 1; i++ ) {
+      if ( array[i] > array[i+1] ) {
+        temp = array[i];
+        array[i] = array[i+1];
+        array[i+1] = temp;
+        swaps++;
+      }
+    }
+  }
+
+  return array
+}
+
+// console.log( bubbleSort( [2,6,1,7,2,6,4] ) );
+
+function mergeSort( array ) {
+  if ( array.length === 1 ) {
+    return array;
+  }
+
+  half = Math.floor( array.length / 2 );
+
+  var array1 = array.slice(0, half)
+  var array2 = array.slice(half)
+
+  return merge( mergeSort( array1 ), mergeSort( array2 ) );
+}
+
+function merge( array1, array2 ) {
+  var mergedArray = [];
+
+  while ( array1.length !== 0 || array2.length !== 0 ) {
+    if ( array1.length === 0 ) {
+      mergedArray.push( array2.shift() );
+    } else if ( array2.length === 0 ) {
+      mergedArray.push( array1.shift() );
+    } else if ( array1[0] > array2[0] ) {
+      mergedArray.push( array2.shift() );
+    } else {
+      mergedArray.push( array1.shift() );
+    }
+  }
+
+  return mergedArray;
+}
+
+// console.log( mergeSort( [2,6,1,7,2,6,4] ) );
+
+function quickSort( array ) {
+  
+}
