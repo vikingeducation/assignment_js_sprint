dc33c64ff8139668340e8ff9dc26d8722b5fef93
Kit Langton <kitlangton@gmail.com>
2016-03-07 18:07:53 -0500

    jquery thing

--- checkers.js
+++ checkers.js
@@ -1,6 +1,6 @@
 function Piece(color) {
   this.color = color;
-} 
+}
 
 
 function Checkers() {
@@ -8,20 +8,76 @@ function Checkers() {
   this.board = new Array(8);
 
   this.buildBoard = function() {
-
     for (var i = 0; i < 8 ; i++) {
       this.board[i] = new Array(8);
-      
-      for (var j = 0; i < 8 ; j++) {
-        if ((i + j) % 2 == 0 ) {
+
+      for (var j = 0; j < 8 ; j++) {
+        if ((i + j) % 2 == 1 ) {
           if (i < 3) {
-            this.board[i][j] = new Piece("B");
+            this.board[i][j] = new Piece("red");
           } else if (i > 4) {
-            this.board[i][j] = new Piece("W");
+            this.board[i][j] = new Piece("black");
           }
-        }   
+        }
       }
     }
+  };
+
+  this.pieceAt = function(row, col) {
+    return this.board[row][col];
   }
+
+  this.colorAt = function(row, col) {
+    var piece = this.board[row][col];
+    if (piece) {
+      return piece.color;
+    } else {
+      return "";
+    }
+  };
+
+  this.move = function(from, to) {
+    this.board[to[0]][to[1]] = this.pieceAt(from[0], from[1]);
+    this.board[from[0]][from[1]] = null;
+  }
+
   this.buildBoard();
-} 
+}
+
+var renderBoard = function(game) {
+  $(".board").html("");
+  for (var i = 0; i < game.board.length; ++i) {
+    for (var j = 0; j < game.board[i].length; ++j) {
+      var space = $($.parseHTML("<div class='space'></div>"));
+      space.data("row", i);
+      space.data("col", j);
+      if ( (i + j) % 2 == 0 ) {
+        space.addClass('even');
+      }
+      var piece = game.pieceAt(i,j);
+      if (piece) {
+        space.append("<div class='piece " + game.colorAt(i,j) + "'></div>");
+      }
+      $(".board").append(space);
+    }
+      $(".board").append("<br>");
+  }
+};
+
+$(document).ready(function() {
+  window.game = new Checkers();
+  renderBoard(game);
+
+  var from = null;
+  $('.board').on('click', '.space', function(event) {
+    if (from) {
+      var to = [$(this).data('row'), $(this).data('col')];
+      window.game.move(from, to);
+      renderBoard(game);
+      from = null;
+    } else {
+      from = [$(this).data('row'), $(this).data('col')];
+    }
+  });
+});
+
