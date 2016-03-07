0bde6b358167412f773bb6f821d7a7c60e256eaa
Joshua Masland-Sarani <j.masland@gmail.com>
2016-03-07 12:32:38 -0500

    first three exercises

--- scripts.js
+++ scripts.js
@@ -1,32 +1,51 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
-    // your code here
-  },
-  
-  reversed: function(){  
-    // your code here
-  },
-
-  loudSnakeCase: function(){  
-    // your code here
+  largestEl: function( array ){
+    var largest = array[0];
+    for ( var i = 1; i < array.length; i++ ) {
+      if ( array[i] > largest ) {
+        largest = array[i];
+      }
+    }
+    return largest;
   },
 
-  compareArrays: function(){ 
+  reversed: function( string ){
+    reversed_string = "";
+    for ( var i = string.length - 1; i >= 0; i-- ) {
+      reversed_string += string[i];
+    }
+    return reversed_string
+  },
+
+  loudSnakeCase: function( string ){
+    console.log(string);
+    loudSnakeString = "";
+    wordArr = string.split(/ +/);
+
+    for ( var i = 0; i < wordArr.length; i++ ) {
+      wordArr[i] = wordArr[i].split(/\W/).join("");
+      wordArr[i] = wordArr[i].charAt(0).toUpperCase() + wordArr[i].slice(1);
+    }
+    console.log(wordArr.join("_"));
+    return loudSnakeString = wordArr.join("_");
+  },
+
+  compareArrays: function(){
     // your code here (replace the return)
-    return "Finish compareArrays first!" 
+    return "Finish compareArrays first!"
   },
 
-  fizzBuzz: function(){  
+  fizzBuzz: function(){
     // your code here
   },
 
-  myMap: function(){  
+  myMap: function(){
     // your code here
   },
 
-  primes: function(){  
+  primes: function(){
     // your code here
   },
 }
