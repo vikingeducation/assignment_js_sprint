b099d8d9fe158c3a680d0e822040053cc7a6b293
Sam O'Keefe <samok13@gmail.com>
2016-03-07 14:39:02 -0800

    attempts at merge sort

--- sort.js
+++ sort.js
@@ -28,11 +28,20 @@ console.log(bubble_sort([5, 3, 8, 1, 0]));
 //merge sort
 
 var merge = function(array_left, array_right){
-  if (array_left.length === 0){
-    array_right;
+  var left = [array_left[0]];
+  var right = [array_right[0]];
+
+  if (typeof array_right !== 'undefined'){
+    return array_right;
   }
-  if (array_right.length === 0){
-    array_left;
+  else if (typeof array_left !== 'undefined'){
+    return array_left;
+  }
+  else if (array_left[0] < array_right[0]){
+    return left.push(merge( array_left.slice(1, array_left.length), array_right ));
+  }
+  else {
+    return right.push(merge( array_left, array_right.slice(1, array_right.length)));
   }
 }
 
@@ -44,7 +53,8 @@ var merge_sort = function(array){
   }
 
   //split array
-  var left_upper_bound = var right_lower_bound = array.length/2;
+  var left_upper_bound = parseInt(array.length/2);
+  var right_lower_bound = parseInt(array.length/2);
 
   //if array length is even
   if(array.length % 2 === 0){
@@ -56,9 +66,13 @@ var merge_sort = function(array){
 
   //defining left and right sides recursively
   var array_left = merge_sort(array.slice(0,left_upper_bound+1));
-  var array_right = merge_sort(array.slice(right_lower_bound, array.length));
-
+  console.log("left" + array_left);
+  var array_right = merge_sort(array.slice(right_lower_bound));
+  console.log("right" + array_right);
+  
   //merge sides together
-  merge(array_left, array_right);
+  return merge(array_left, array_right);
 
 }
+  
+console.log(merge_sort([1,3,7,-2,0,5,8,-9,2,3,4]));
