070de4beca0b6f80e750c380c0f604feeb0d657b
Andrew Baik <and.baik@gmail.com>
2016-03-07 13:10:20 -0500

    reversed, loudsnake, and comparearrays functions done

--- scripts.js
+++ scripts.js
@@ -1,28 +1,56 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(input_array){
+  largestEl: function(inputArray){
     var max = 0;
-    var array_length = input_array.length;
-    for(var i = 0; i < array_length; i++) {
-      if (input_array[i] > max) {
-        max = input_array[i];
+    var arrayLength = inputArray.length;
+    for (var i = 0; i < arrayLength; i++) {
+      if (inputArray[i] > max) {
+        max = inputArray[i];
       }
     }
     return max;
   },
 
-  reversed: function(){
-    // your code here
+  reversed: function(inputString){
+    var max = inputString.length;
+    var result = "";
+    for (var i = max - 1; i >= 0; i--) {
+      result += inputString[i];
+    }
+    return result;
   },
 
-  loudSnakeCase: function(){
-    // your code here
+  loudSnakeCase: function(inputString){
+    /*
+    var words = input_string.split(" ");
+    var max = words.length;
+    var result = "";
+    for (var i = 0; i < max; i++) {
+      if (words[i]){
+        result += "_";
+        result += words[i][0].toUpperCase();
+        result += words[i].substr(1).replace(/(\W+)/g, "");
+      }
+    }
+    return result.substr(1);
+    */
+    var result = inputString.replace(/(\W+)/g, "_");
+    result = result.replace(/_./g, function(v) { return "_" + v[1].toUpperCase() });
+    result = result.substr(0, result.length - 1);
+    return result;
   },
 
-  compareArrays: function(){
-    // your code here (replace the return)
-    return "Finish compareArrays first!"
+  compareArrays: function(array1, array2){
+    var max1 = array1.length;
+    var max2 = array2.length;
+    if (max1 !== max2) return false;
+    else {
+      for (var i = 0; i < max1; i++) {
+        if (array1[i] !== array2[i]) return false;
+      }
+    }
+    return true;
   },
 
   fizzBuzz: function(){
