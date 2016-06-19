cd0f22f86d9e70c3b189462853a52ba1f1bca0b0
Steven Chang <prime_pork@hotmail.com>
2016-06-19 15:55:42 +1000

    LargestEl, Reversed and LoudSnakeCase specs have passed

--- scripts.js
+++ scripts.js
@@ -1,16 +1,56 @@
+
+
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
-    // your code here
+  largestEl: function( array ) {  
+    var largestElement = 0;
+    for(var i = 0; i < array.length; i++) {
+      if (array[i] > largestElement) {
+        largestElement = array[i];
+      };
+    };
+    return largestElement;
   },
   
-  reversed: function(){  
-    // your code here
+  reversed: function( string ){  
+    return string.split("")
+                 .reverse()
+                 .join("");
   },
 
-  loudSnakeCase: function(){  
-    // your code here
+  loudSnakeCase: function( string ){
+    var splitString = string.split(" ");
+    var filteredSplitString = [];
+    // Function to see if a character is a letter
+    function isLetter(str) {
+      return !!(str.length === 1 && str.match(/[a-z]|[A-Z]/));
+    };
+
+    // Getting rid of empty strings
+    for (var i = 0; i < splitString.length; i++) {
+      if (splitString[i].length > 0) {
+        filteredSplitString.push(splitString[i]);
+      };
+    };
+
+    // Getting rid of all the punctuation
+    for (var i = 0; i < filteredSplitString.length; i++) {
+      var onlyLetters = [];
+      for (var j = 0; j < filteredSplitString[i].length; j++) {
+        // So write now we're going through each word right...
+        // and we need to go throuch each letter.
+        var character = filteredSplitString[i][j];
+        if (!!(character.match(/[a-z]|[A-Z]/))) {
+          onlyLetters.push(character);
+        };
+      };
+      onlyLetters[0] = onlyLetters[0].toUpperCase();
+      filteredSplitString[i] = onlyLetters.join("");
+    };
+
+    // Now joining everything with an underscore
+    return filteredSplitString.join("_");
   },
 
   compareArrays: function(){ 
