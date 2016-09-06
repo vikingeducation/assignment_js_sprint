6d12c6852fd7cf4c11e42f10ea92618ea6674841
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-09-06 09:26:20 -0700

    Finished reversed

--- scripts.js
+++ scripts.js
@@ -1,16 +1,35 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
+  largestEl: function(array){  
     // your code here
+    return array.sort()[array.length - 1];
   },
   
-  reversed: function(){  
+  reversed: function(string){  
     // your code here
+    var rev_string = "";
+
+    for (var i = string.length - 1; i >= 0; i--) {
+      rev_string += string[i];
+    }
+
+    return rev_string;
   },
 
-  loudSnakeCase: function(){  
-    // your code here
+  loudSnakeCase: function(sentence){  
+    var words = sentence.split(" ");
+
+    words.forEach( function(word, index) {
+      var new_word = "";
+      for (var i = 0; i < word.length; i++) {
+        if ('A' <= word[i] <= 'Z' ||  'a' <= word[i] <= 'z')
+          new_word += word[i];
+      }
+      words[index] = new_word;
+    })
+    
+    return words.join("_")
   },
 
   compareArrays: function(){ 
