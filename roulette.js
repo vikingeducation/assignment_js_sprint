f04871f5d2a994fffce54a67fc1d97c12f4ae571
Leo Ahnn <leo.ahnn@gmail.com>
2016-09-06 13:53:41 -0400

    working roulette alternate inputs

--- roulette.js
+++ roulette.js
@@ -4,13 +4,78 @@ function Roulette (bankRoll) {
   this.bankRoll = function () {
     return this.value;
   };
-  this.spin = function(bet, num) {
-    var rouletteNum = Math.floor((Math.random() * 38)+1);
-    switch (rouletteNum) {
-      (num === rouletteNum) {
-        this.value = this.value + (bet * 35);
-      } else {
-        this.value = this.value - bet;
+  this.spin = function(bet, input) {
+    var rouletteNum = Math.floor(Math.random() * 38);
+    switch (input) {
+      case '00':
+        if (rouletteNum === 37) {
+          this.value = this.value + (bet * 35);
+        } else {
+          this.value = this.value - bet;
+        }
+        break;
+      case 'Even':
+        if (rouletteNum % 2 === 0) {
+          this.value = this.value + bet;
+        }
+        else {
+          this.value = this.value - bet;
+        }
+        break;
+      case 'Odd':
+        if (rouletteNum % 2 !== 0) {
+          this.value = this.value + bet;
+        }
+        else {
+          this.value = this.value - bet;
+        }
+        break;
+      case '1 to 18':
+        if (rouletteNum <= 18 && rouletteNum !== 0) {
+          this.value = this.value + bet;
+        }
+        else {
+          this.value = this.value - bet;
+        }
+        break;
+      case '19 to 36':
+        if (rouletteNum > 18 && rouletteNum !== 37) {
+          this.value = this.value + bet;
+        }
+        else {
+          this.value = this.value - bet;
+        }
+        break;
+      case '1st 12':
+        if (rouletteNum <= 12 && rouletteNum !== 0) {
+          this.value = this.value + (bet * 2);
+        }
+        else {
+          this.value = this.value - bet;
+        }
+        break;
+      case '2nd 12':
+        if (rouletteNum > 12 && rouletteNum < 25) {
+          this.value = this.value + (bet * 2);
+        }
+        else {
+          this.value = this.value - bet;
+        }
+        break;
+      case '3rd 12':
+        if (rouletteNum > 24 && rouletteNum < 37) {
+          this.value = this.value + (bet * 2);
+        }
+        else {
+          this.value = this.value - bet;
+        }
+        break;
+      default:
+        if (input === rouletteNum) {
+          this.value = this.value + (bet * 35);
+        } else {
+          this.value = this.value - bet;
+        }
       }
     return this.value;
   };
@@ -21,8 +86,8 @@ function Roulette (bankRoll) {
 }
 
 var r = new Roulette(1000);
-r.spin(10,24);
+r.spin(10,'3rd 12');
 console.log(r.value);
-r.spin(10,24);
+r.spin(10,'Odd');
 console.log(r.value);
 console.log(r.buyIn(390));
