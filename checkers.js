f87272c46188a0d95f0a817622133b6fa12bf7cb
Leo Ahnn <leo.ahnn@gmail.com>
2016-09-06 17:31:04 -0400

    working on validation for moves

--- checkers.js
+++ checkers.js
@@ -6,14 +6,15 @@ function Checkers() {
       for (var i = 0; i < 8; i++) {
         this.board.push(new Array(8));
         for (var j = 0; j < 8; j++) {
-          this.board[i][j] = " ";
+          this.board[i][j] = "";
         }
       }
     }
 
     function arrangeMarkers() {
       var color = 'X';
-      function changeColor (row) {
+
+      function changeColor(row) {
         return function(ele, index) {
           if (row % 2 === 0 && index % 2 !== 0) {
             return color;
@@ -47,9 +48,9 @@ function Checkers() {
       var rowString = '';
       for (var j = 0; j < this.board[i].length; j++) {
         if (this.board[i][j]) {
-          rowString += this.board[i][j];
+          rowString += '|'+this.board[i][j]+'|';
         } else {
-          rowString += '_';
+          rowString += '|_|';
         }
       }
       stringified += rowString + "\n";
@@ -57,7 +58,32 @@ function Checkers() {
     console.log(stringified);
   };
 
-  this.move = function(from, to) {
+  this.move = function(from_row, from_col, to_row, to_col) {
+    function validiate() {
+      if((to_row > this.board.length && to_row < 0) && (to_col > this.board[0].length && to_col < 0)) {
+        return false;
+      } else if(this.board[to_row][to_col]) {
+        return false;
+      } else if(Math.abs(from_row - to_row) > 2 || Math.abs(from_col - to_col) > 2) {
+        return false;
+      }
+    }
+    //check if own piece
+    if (this.board[from_row][from_col] === this.turn) {
+      // if X's then only move in (+) row direction
+      if (this.turn === 'X') {
+        // up one row, to the left and right
+        if (to_row || )
+        // if enemy occupies one of those squares, check up two rows and diagonal over the enemy.
+      }
+    } else {
+      console.log ("can't move that piece")
+    }
+
+
+
+// if O'x then only move in (-) row direction
+
 
   };
 }
