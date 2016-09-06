51e259c42581911094764d1a6db1c72a07168d7f
Alex Lach <alexglach@gmail.com>
2016-09-06 12:48:27 -0400

    working on loud camel case

--- scripts.js
+++ scripts.js
@@ -13,14 +13,34 @@ var sprintFunctions = {
   
   reversed: function(string){  
     var reverse = "";
-    for (var i = string.length - 1; i > 0; i--) {
-      var reverse = reverse.concat(string[i]);
+    for (var i = string.length - 1; i >= 0; i--) {
+      reverse += string[i];
     };
     return reverse;
   },
 
-  loudSnakeCase: function(){  
+  loudSnakeCase: function(string){  
     // your code here
+    var return_string = "";
+    for (var i = 0; i < string.length; i++) {
+      var letter = string[i].toLowerCase();
+      if (letter.match(/[a-z]/)) {
+        if (return_string[-1] === "_") {
+          return_string += string[i].toUpperCase();
+        }
+        else{
+          return_string += string[i].toLowerCase();
+        };
+      }
+      else {
+        if (return_string[-1] !== "_") {
+          if (i !== string.length - 1) {
+            return_string += "_"; 
+          };
+        };
+      };
+    };
+    return return_string;
   },
 
   compareArrays: function(){ 
