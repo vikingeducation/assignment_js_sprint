dd6efe3c4f125483206e11010b2e4ad21546e331
David Jiang <davidjgrey@gmail>
2016-09-06 09:51:59 -0700

    working on myMap

--- scripts.js
+++ scripts.js
@@ -12,24 +12,51 @@ var sprintFunctions = {
 
   loudSnakeCase: function(sentence){
     // your code here
-    words = sentence.replace(/[^\x00-\x7F]/g, "").split(" ");
-    words.map(function(word) { toUpperCase(word[0]); });
+    words = sentence.replace(/[^\w\s]|_/g, "")
+         .replace(/\s+/g, " ").split(" ");
+    new_words = words.map(function(word) { 
+                return word[0].toUpperCase() + word.substring(1).toLowerCase();
+              });
 
-    return words.join("_")
+    return new_words.join("_")
 
   },
 
-  compareArrays: function(){
-    // your code here (replace the return)
-    return "Finish compareArrays first!"
+  compareArrays: function(arr_1, arr_2){
+    if (arr_1.length !== arr_2.length){
+      return false;
+    }
+    for (var i = 0; i < arr_1.length; i++){
+      if (arr_1[i] !== arr_2[i]){
+        return false;
+      }
+    }
+    return true;
   },
 
-  fizzBuzz: function(){
-    // your code here
+  fizzBuzz: function(input){
+    var result = []
+    for (var i = 1; i <= input; i++){
+      if (i%15===0){
+        result[i-1] = "FIZZBUZZ";
+      }
+      else if (i%5===0){
+        result[i-1] = "BUZZ";
+      }
+      else if (i%3===0){
+        result[i-1] = "FIZZ";
+      }
+      else{
+        result[i-1] = i;
+      }
+    }
+    return result
   },
 
-  myMap: function(){
-    // your code here
+  myMap: function(arr, callback){
+    //var result = [];
+    arr = arr.forEach(callback);
+    return arr;
   },
 
   primes: function(){
