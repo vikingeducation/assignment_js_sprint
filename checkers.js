af08ce7ca457ff17a180a2b27c28e19d50a712c7
cjvirtucio87 <cjv287@gmail.com>
2016-09-06 11:49:51 -0700

    Added stringified display.

--- checkers.js
+++ checkers.js
@@ -6,35 +6,62 @@ function Checkers() {
       this.board.push(new Array(8));
     }
     var color = 'X';
-    for (var i = 0; i < 3; i++) {
-      this.board[i] = this.board[i].map(function(ele, index) {
-        console.log(index);
-        if (i % 2 === 0 && index % 2 !== 0) {
-          return color;
-        } else if (i % 2 !== 0 && index % 2 === 0) {
-          return color;
+    for (var topIndex = 0; topIndex < 3; topIndex++) {
+      for (var topRow = 0; topRow < this.board[topIndex].length; topRow++) {
+        if (topIndex % 2 === 0 && topRow % 2 !== 0) {
+          this.board[topIndex][topRow] = color;
+        } else if (topIndex % 2 !== 0 && topRow % 2 === 0) {
+          this.board[topIndex][topRow] = color;
         }
-      });
+      }
     }
-    color = 'O'
-    for (var i = 5; i < 8; i++) {
-      this.board[i] = this.board[i].map(function(ele, index) {
-        if (i % 2 === 0 && index % 2 !== 0) {
-          return color;
-        } else if (i % 2 !== 0 && index % 2 === 0) {
-          return color;
+    var color = 'O';
+    for (var bottomIndex = 5; bottomIndex < 8; bottomIndex++) {
+      for (var bottomRow = 0; bottomRow < this.board[bottomIndex].length; bottomRow++) {
+        if (bottomIndex % 2 === 0 && bottomRow % 2 !== 0) {
+          this.board[bottomIndex][bottomRow] = color;
+        } else if (bottomIndex % 2 !== 0 && bottomRow % 2 === 0) {
+          this.board[bottomIndex][bottomRow] = color;
         }
-      });
+      }
     }
+
+    // var changeColor = function(ele, index) {
+    //   console.log(index);
+      // if (i % 2 === 0 && index % 2 !== 0) {
+      //   return color;
+      // } else if (i % 2 !== 0 && index % 2 === 0) {
+      //   return color;
+      // }
+    // };
+    // for (var topIndex = 0; topIndex < 3; topIndex++) {
+    //   this.board[topIndex] = this.board[topIndex].map(changeColor);
+    // }
+    // color = 'O';
+    // for (var bottomIndex = 5; bottomIndex < 8; bottomIndex++) {
+    //   this.board[bottomIndex] = this.board[bottomIndex].map(changeColor);
+    // }
   };
 
   this.display = function() {
-    console.log(this.board);
-  }
+    var stringified = '';
+    for (var i = 0; i < this.board.length; i++) {
+      var rowString = '';
+      for (var j = 0; j < this.board[i].length; j++) {
+        if (this.board[i][j]) {
+          rowString += this.board[i][j];
+        } else {
+          rowString += '_';
+        }
+      }
+      stringified += rowString + "\n";
+    }
+    console.log(stringified);
+  };
 
   this.move = function(from, to) {
 
-  }
+  };
 }
 
 var c = new Checkers();
--- scripts.js
+++ scripts.js
@@ -21,9 +21,10 @@ var sprintFunctions = {
   },
 
   loudSnakeCase: function(str){
+    // str = str.replace(/[\W\S]/g,"");
     str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
     str = toTitleCase(str);
-    str = str.replace(/\s+/g, '_')
+    str = str.replace(/\s+/g, '_');
 
     function toTitleCase(str) {
       return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
@@ -84,26 +85,3 @@ var sprintFunctions = {
     return output;
   },
 };
-
-function bubbleSort(arr) {
-  var counter = 1;
-  while (counter !== 0) {
-    // Boolean. Stop swapping when true.
-    counter = 0;
-    // Go through array
-    for (var i = 0; i < arr.length-1; i++) {
-      var j = i+1;
-      if (arr[j] < arr[i]) {
-        // Swap and set boolean to true.
-        var temp = arr[j];
-        arr[j] = arr[i];
-        arr[j] = temp;
-        counter++;
-      }
-    }
-  }
-  return arr;
-}
-
-var array = [5,2,3,4,1];
-console.log(bubbleSort(array));
