96036e15be1f5175e266ed13ef397fc5517186d4
Pramod Jacob <psjacob68@gmail.com>
2017-01-03 11:48:13 -0500

    finished loudSnakeCase

--- scripts.js
+++ scripts.js
@@ -1,32 +1,48 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
-    // your code here
-  },
-  
-  reversed: function(){  
-    // your code here
-  },
-
-  loudSnakeCase: function(){  
-    // your code here
+  largestEl: function(array){
+    largest = array[0];
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] > largest) {
+        largest = array[i]
+      }
+    }
+    return largest;
   },
 
-  compareArrays: function(){ 
+  reversed: function(string){
+    string_array = string.split('');
+    return string_array.reverse().join('');
+  },
+
+  loudSnakeCase: function(phrase){
+    phrase = phrase.replace(/[^\w\s]/g, '');
+    phrase = phrase.replace(/\s+/g, ' ')
+    phrase_array = phrase.split('');
+    phrase_array[0] = phrase_array[0].toUpperCase();
+    while (phrase_array.indexOf(' ') > -1) {
+      index_value = phrase_array.indexOf(' ');
+      phrase_array[index_value] = '_';
+      phrase_array[index_value + 1] = phrase_array[index_value + 1].toUpperCase();
+    }
+    return phrase_array.join('');
+  },
+
+  compareArrays: function(){
     // your code here (replace the return)
-    return "Finish compareArrays first!" 
+    return "Finish compareArrays first!"
   },
 
-  fizzBuzz: function(){  
+  fizzBuzz: function(){
     // your code here
   },
 
-  myMap: function(){  
+  myMap: function(){
     // your code here
   },
 
-  primes: function(){  
+  primes: function(){
     // your code here
   },
 }
