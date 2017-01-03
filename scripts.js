6fe071ecbda5bf60e23fe0a33e7c007174fcbea9
Jonathan Popenuck <john.popenuck@gmail.com>
2017-01-03 08:51:54 -0800

    started on loudsnakecase for javascript sprint

--- scripts.js
+++ scripts.js
@@ -1,32 +1,55 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
-    // your code here
-  },
-  
-  reversed: function(){  
-    // your code here
-  },
-
-  loudSnakeCase: function(){  
-    // your code here
+  largestEl: function(array){
+    var largest = 0;
+    array.forEach(function(element){
+      if (element > largest) {
+        largest = element;
+      };
+    });
+    return largest;
   },
 
-  compareArrays: function(){ 
+  reversed: function(string){
+    var rstring = "";
+    for (i = string.length - 1; i >= 0; i--) {
+      rstring += string[i];
+    };
+    return rstring;
+  },
+
+  loudSnakeCase: function(string){
+    var lsc = ""
+    for (i = 0; i < string.length; i++) {
+      if (string[i].match(/W/)) {
+        lsc[i] = "";
+      } else if (string[i] === " ") {
+        lsc[i] = "_";
+      } else {
+        lsc[i] = string[i];
+      };
+      if (string[i - 1] === "_") {
+        lsc[i] = lsc[i].toUpperCase();
+      };
+    };
+    console.log(lsc);
+  },
+
+  compareArrays: function(){
     // your code here (replace the return)
-    return "Finish compareArrays first!" 
+    return "Finish compareArrays first!"
   },
 
-  fizzBuzz: function(){  
+  fizzBuzz: function(){
     // your code here
   },
 
-  myMap: function(){  
+  myMap: function(){
     // your code here
   },
 
-  primes: function(){  
+  primes: function(){
     // your code here
   },
 }
