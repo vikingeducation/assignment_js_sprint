30b937c290b3fdeb18613f9f88e9b41a8285785a
Alexa <alexa.anderson@outlook.com>
2017-01-03 15:52:14 -0600

    merge sort

--- bigger_problems.js
+++ bigger_problems.js
@@ -17,57 +17,44 @@ var bubbleSort = function(array) {
 
 bubbleSort([5,2,7,9,4,0,-7])
 
-var mergeSort = function(array) {
-  var combine = function(array1, array2) {
-    var combinedArray = [];
-    var index1 = 0;
-    var index2 = 0;
-    if (array1 && array2) {
-      while (index1 < array1.length && index2 < array2.length) {
-        if (array1[index1] < array2[index2]) {
-          combinedArray.push(array1[index1]);
-          index1++;
-        } else {
-          combinedArray.push(array2[index2]);
-          index2++;
-        }
-      };
-      if (index1 < array1.length) {
-        for (var i = index1; i < array1.length; i++) {
-          combinedArray.push(array1[i]);
-        };
-      };
-      if (index2 < array2.length) {
-        for (var i = index2; i < array2.length; i++) {
-          combinedArray.push(array2[i]);
-        };
-      };
-    } else if (array1) {
-      for (var i = 0; i < array1.length; i++) {
-        combinedArray.push(array1[i]);
-      }
-    } else {
-      for (var i = 0; i < array2.length; i++) {
-        combinedArray.push(array2[i]);
-      };
-    }
-    console.log(combinedArray);
-    return combinedArray
-  };
+var mergeSort = function(start_array) {
+  split_arr(start_array)
+};
 
-  var split = function(array) {
-    if (array.length <= 2) {
-      console.log(array)
-      return array
+var combine = function(array1, array2) {
+  var combinedArray = [];
+
+  while (array1[0] && array2[0]) {
+    if (array1[0] <= array2[0]) {
+      console.log(`combined array ${combinedArray}`)
+      combinedArray.push(array1.shift())
     } else {
-      array1 = array.splice(0, Math.floor(array.length / 2))
-      array2 = array
-      console.log(`array1: ${array1}`)
-      console.log(`array2: ${array2}`)
-      var result = combine(split(array1), split(array2))
+      console.log(`combined array ${combinedArray}`)
+
+      combinedArray.push(array2.shift())
     }
+  }
+  array1.forEach(function(num) {
+    combinedArray.push(num)
+  })
+  array2.forEach(function(num) {
+    combinedArray.push(num)
+  })
+  console.log(`combined array ${combinedArray}`)
+  return combinedArray
+};
+
+var split_arr = function(array) {
+  if (array.length <= 1) {
+    console.log(array)
+    return array
+  } else {
+    var array1 = array.splice(0, Math.floor(array.length / 2))
+    var array2 = array
+    console.log(`array1: ${array1}`)
+    console.log(`array2: ${array2}`)
+    var result = combine(split_arr(array1), split_arr(array2))
     console.log(result);
     return result
-  };
-  split(array)
+  }
 };
