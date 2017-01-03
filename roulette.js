8546e9cd89c9ff924869415f477862c24f4b1f66
Jessica G <gillan.jessica@gmail.com>
2017-01-03 16:17:18 -0700

    case statements

--- roulette.js
+++ roulette.js
@@ -9,10 +9,48 @@ function Roulette(startingAmount) {
     this.bankrollAmount += amount;
   };
 
-  this.spin = function(bet, num) {
+  this.spin = function(bet, input) {
     var spinResult = Math.floor(Math.random() * 35) + 1
-    if (num === spinResult) {
-      this.result(bet * 35, spinResult, "win");
+
+    var multiplier,
+        match;
+    switch( input ){
+      case "0":
+        multiplier = 35;
+        match = [0];
+        break;
+      case "00":
+        multiplier = 35;
+        match = [37];
+        break;
+      case "1 to 18":
+        multiplier = 1;
+        match = this.arrayBuilder(1,18);
+        break;
+      case "19 to 36":
+        multiplier = 1;
+        break;
+      case "Even":
+        multiplier = 1;
+        break;
+      case "Odd":
+        multiplier = 1;
+        break;
+      case "1st 12":
+        multiplier = 2;
+        break;
+      case "2nd 12":
+        multiplier = 2;
+        break;
+      case "3rd 12":
+        multiplier = 2;
+        break;
+      default:
+        multiplier = 35;
+    }
+    if (input === spinResult) {
+      payoutAmnt = this.payout(bet, input)
+      this.result( payoutAmnt, spinResult, "win");
     } else {
       this.result(-bet, spinResult, "lose");
     }
