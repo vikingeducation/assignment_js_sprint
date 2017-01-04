16a2787a6fa76b877a7696b9dc821a0a3de663d1
Francisco Suero <suerofrancisco@yahoo.com>
2017-01-03 19:00:54 -0500

    done?

--- roulette.js
+++ roulette.js
@@ -11,11 +11,15 @@ var roulette = {
     this.lostScreen = document.getElementById("lost-screen");
     this.resultScreen = document.getElementById("result");
     this.betInput.addEventListener("change", function(){this.showBet()}.bind(this));
+    this.updateScreen(0);
+    this.bankScreen.innerHTML = this.bank;
   },
 
   restart: function(){
-    this.lostScreen.classList.remove();
+    this.lostScreen.classList.remove("active");
     this.init();
+    $('.wheel img.board').css('transform', 'rotate(0deg)');
+
   },
 
   showBet: function(){
@@ -23,6 +27,10 @@ var roulette = {
   },
 
   setBet: function(){
+    if(+this.bankInput.value < 0){
+      this.bankInput.value = +this.bankInput.value * -1 
+    }
+
     if(+this.bankInput.value > this.bank)
       return this.bank;
     else
@@ -30,8 +38,9 @@ var roulette = {
 
   },
 
-  payOut: function(amount){
-    this.bank += amount * 36;
+  payOut: function(amount, multiplier){
+
+    this.bank += amount * multiplier;
   },
 
   lose: function(amount){
@@ -49,13 +58,42 @@ var roulette = {
   collectBet: function(){
     var betValue = this.betInput.value;
     var betAmount = this.setBet();
-    if(this.checkResults(betValue, this.spin())){
-      this.payOut(betAmount);
+    if(this.checkResults(betValue, this.spin().value)){
+      this.payOut(betAmount, 36);
     } else {
       this.lose(betAmount);
     }
 
-    this.bankScreen.innerHTML = this.bank;
+    this.updateBank()
+  },
+
+  betColor: function(color){
+    var betAmount = this.setBet();
+    if(this.checkResults(color, this.spin().color)){
+      this.payOut(betAmount, 1);
+    } else {
+      this.lose(betAmount);
+    }
+
+    this.updateBank()
+  },
+
+  betParity: function(parity){
+    var betAmount = this.setBet();
+    if(this.checkResults(+parity, this.spin().value % 2)){
+      this.payOut(betAmount, 1);
+    } else {
+      this.lose(betAmount);
+    }
+
+    this.updateBank()
+
+  },
+
+  updateBank: function(){
+    setTimeout(function(){
+      this.bankScreen.innerHTML = this.bank;
+    }.bind(this), 2000)
   },
 
   checkResults: function(choice, outcome){
@@ -65,13 +103,12 @@ var roulette = {
       return false;
   },
 
-
   spin: function(){
     var pos = Math.round(Math.random()*37);
     $('.wheel img.board').css('transition', 'all 0s');
     $('.wheel img.board').css('transform', 'rotate(-' + (deg % 360) + 'deg)');
     setTimeout(function(){roulette.animateWheel(pos)}, 100);
-    setTimeout(function(){this.updateScreen(this.positions[pos])}.bind(this), 2000)
+    this.updateScreen(this.positions[pos].value);
     return this.positions[pos];
   },
 
@@ -82,8 +119,11 @@ var roulette = {
   },
 
   updateScreen: function(num){
-    this.resultScreen.innerHTML = num;
+    setTimeout(function(){
+      this.resultScreen.innerHTML = num;}.bind(this), 
+      2000)
   },
+
 
   betScreen: null,
   betInput: null,
@@ -93,43 +133,43 @@ var roulette = {
   resultScreen: null,
 
   positions: {
-    0: 0,
-    1: 32,
-    2: 15,
-    3: 19,
-    4: 4,
-    5: 21,
-    6: 2,
-    7: 25,
-    8: 17,
-    9: 34,
-    10: 6,
-    11: 27,
-    12: 13,
-    13: 36,
-    14: 11,
-    15: 30,
-    16: 8,
-    17: 23,
-    18: 10,
-    19: 5,
-    20: 24,
-    21: 16,
-    22: 33,
-    23: 1,
-    24: 20,
-    25: 14,
-    26: 31,
-    27: 9,
-    28: 22,
-    29: 18,
-    30: 29,
-    31: 7,
-    32: 28,
-    33: 12,
-    34: 35,
-    35: 3,
-    36: 26,
+    0: {value: 0, color: "green" }, 
+    1: {value: 32, color: "red" }, 
+    2: {value: 15, color: "black" }, 
+    3: {value: 19, color: "red" }, 
+    4: {value: 4, color: "black" },
+    5: {value: 21, color: "red" }, 
+    6: {value: 2, color: "black" }, 
+    7: {value: 25, color: "red" }, 
+    8: {value: 17, color: "black" }, 
+    9: {value: 34, color: "red" }, 
+    10: {value: 6, color: "black" }, 
+    11: {value: 27, color: "red" }, 
+    12: {value: 13, color: "black" }, 
+    13: {value: 36, color: "red" }, 
+    14: {value: 11, color: "black" }, 
+    15: {value: 30, color: "red" }, 
+    16: {value: 8, color: "black" }, 
+    17: {value: 23, color: "red" }, 
+    18: {value: 10, color: "black" }, 
+    19: {value: 5, color: "red" }, 
+    20: {value: 24, color: "black" }, 
+    21: {value: 16, color: "red" }, 
+    22: {value: 33, color: "black" }, 
+    23: {value: 1, color: "red" }, 
+    24: {value: 20, color: "black" }, 
+    25: {value: 14, color: "red" }, 
+    26: {value: 31, color: "black" }, 
+    27: {value: 9, color: "red" }, 
+    28: {value: 22, color: "black" }, 
+    29: {value: 18, color: "red" }, 
+    30: {value: 29, color: "black" }, 
+    31: {value: 7, color: "red" }, 
+    32: {value: 28, color: "black" }, 
+    33: {value: 12, color: "red" }, 
+    34: {value: 35, color: "black" }, 
+    35: {value: 3, color: "red" }, 
+    36: {value: 26, color: "black" } 
   }
 }
 
@@ -138,7 +178,20 @@ $(function(){
   $('#bet-btn').on('click', function(){
     roulette.collectBet();
   })
-  $('.restart').on('click', function(){
-    roulette.restart();
+  $('.restart').each(function(){
+    $(this).on('click', function(){
+      roulette.restart();
+    })
+  })
+  $('.bet-color').each(function(){
+    $(this).on('click', function(){
+      roulette.betColor($(this).data("color"));
+    })
+  })
+
+  $('.bet-parity').each(function(){
+    $(this).on('click', function(){
+      roulette.betParity($(this).data("parity"));
+    })
   })
 })
