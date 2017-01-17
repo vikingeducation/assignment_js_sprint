65bd25ddc216cc7031308aaf07c61ab6f78ccd36
Egle Libby <egle.ciparyte@gmail.com>
2017-01-17 17:12:52 -0500

    add loud snake case function

--- scripts.js
+++ scripts.js
@@ -9,8 +9,22 @@ var sprintFunctions = {
     return string.split("").reverse().join("");
   },
 
-  loudSnakeCase: function(){  
-    // your code here
+  loudSnakeCase: function(sentence){
+    var result = "";
+    sentence.split("").forEach(function(element, index) {
+      if (element.match(/[a-z]/i)) {
+        if (result.length === 0 || !result[result.length-1].match(/[a-z]/i)) {
+          result += element.toUpperCase();
+        } else{
+          result += element;
+        }
+      } else{
+        if (result[result.length-1] !== "_" && index != sentence.length - 1) {
+          result += "_";
+        }
+      }
+    });
+    return result;
   },
 
   compareArrays: function(){ 
