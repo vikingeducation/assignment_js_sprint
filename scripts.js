ed173ea74ebc0d0ac47a418125353f9e9a2cc374
Ann Allan <ann.cristina.allan@gmail.com>
2017-01-20 10:51:36 -0500

    Add Largest Element,Reverse, Loud Snake Case, Compare Arrays

--- scripts.js
+++ scripts.js
@@ -1,32 +1,62 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
+  largestEl: function(input){
     // your code here
-  },
-  
-  reversed: function(){  
-    // your code here
+    input.sort;
+    return input[input.length - 1]
   },
 
-  loudSnakeCase: function(){  
+  reversed: function(input){
     // your code here
+    var characters = input.split("");
+    var revs = characters.reverse();
+    var answer = revs.join("");
+    return answer;
   },
 
-  compareArrays: function(){ 
+  loudSnakeCase: function(input){
+    // your code here
+    String.prototype.capitalize = function() {
+       return this.charAt(0).toUpperCase() + this.slice(1);
+     };
+    var capStrings = [];
+   var stripString = input.replace(/(~|`|!|@|#|$|%|^|&|\*|\(|\)|{|}|\[|\]|;|:|\"|'|<|,|\.|>|\?|\/|\\|\||-|_|\+|=)/g,"");
+   var indStrings = stripString.split(" ");
+   indStrings.forEach(function(element){
+     capStrings.push(element.capitalize());
+   });
+   for(var i = 0; i < (capStrings.length -1); i++) {
+     if (capStrings[i] === "") capStrings.splice(i, 1);
+   };
+   var final = capStrings.join("_");
+   return final;
+},
+
+
+  compareArrays: function(input1, input2){
     // your code here (replace the return)
-    return "Finish compareArrays first!" 
+    if (input1.length != input2.length) {
+      return false;
+    } else {
+      for(var i = 0; i < (input1.length - 1); i++) {
+        if (input1[i] != input2[i]) {
+          return false;
+        }
+      }
+    }
+  return true;
   },
 
-  fizzBuzz: function(){  
+  fizzBuzz: function(){
     // your code here
   },
 
-  myMap: function(){  
+  myMap: function(){
     // your code here
   },
 
-  primes: function(){  
+  primes: function(){
     // your code here
   },
 }
