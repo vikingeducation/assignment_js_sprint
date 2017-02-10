3a9a6d7cead16af25f27df2c4fadcdf039d305d2
Renzo Tomlinson <rttomlinson@gmail.com>
2017-02-10 23:39:19 +0000

    Add fizzbuzz function

--- scripts.js
+++ scripts.js
@@ -87,7 +87,22 @@ var sprintFunctions = {
    *
    **/
   fizzBuzz: function fizzBuzz(num) {
-  
+              var arr = [];
+              for (let i = 1; i <= num; i++) {
+                if (i % 15 === 0) {
+                  arr.push("FIZZBUZZ");
+                }
+                else if (i % 3 === 0) {
+                  arr.push("FIZZ");
+                }
+                else if (i % 5 === 0) {
+                  arr.push("BUZZ");
+                }
+                else {
+                  arr.push(i);
+                }
+              }
+              return arr;
   },
 
   /**
