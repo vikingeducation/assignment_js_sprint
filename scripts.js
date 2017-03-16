2a9e69df4c11e3be27712d102cc2e1f7b85407fc
Andrew Knox <aknox0329@gmail.com>
2017-03-16 13:00:36 -0700

    first function complete

--- scripts.js
+++ scripts.js
@@ -1,32 +1,43 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
-    // your code here
+  largestEl: function(array){
+    for (var i = 0; i < array.length; i ++) {
+      var a = 0;
+      var b = 0;
+      if (a < array[i]) {
+        b = array[i];
+      }
+    }
+    return b;
   },
-  
-  reversed: function(){  
+
+  reversed: function(array){
+    var reverseArray = [];
+    for (var i = array.length - 1; i >= 0; i--) {
+      reverseArray.push(array[i]);
+    }
+    return reverseArray;
+  },
+
+  loudSnakeCase: function(){
     // your code here
   },
 
-  loudSnakeCase: function(){  
-    // your code here
-  },
-
-  compareArrays: function(){ 
+  compareArrays: function(){
     // your code here (replace the return)
-    return "Finish compareArrays first!" 
+    return "Finish compareArrays first!"
   },
 
-  fizzBuzz: function(){  
+  fizzBuzz: function(){
     // your code here
   },
 
-  myMap: function(){  
+  myMap: function(){
     // your code here
   },
 
-  primes: function(){  
+  primes: function(){
     // your code here
   },
 }
