a10a89acbdddaf45db7d0987387b24ee73bf0ed9
Christian Florez <florez.christian@gmail.com>
2017-04-04 10:35:58 -0400

    add functions to sprint

--- scripts.js
+++ scripts.js
@@ -1,32 +1,101 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
-    // your code here
+  largestEl: function(arrayToTest){  
+    var largestIndex = 0;
+
+    for (let i = 0; i < arrayToTest.length; i++) {
+      if (arrayToTest[i] > arrayToTest[largestIndex]) {
+        largestIndex = i;
+      }
+    }
+    return arrayToTest[largestIndex];
   },
   
-  reversed: function(){  
-    // your code here
+  reversed: function(string){  
+    return string.split('').reverse('').join('');
   },
 
-  loudSnakeCase: function(){  
-    // your code here
+  loudSnakeCase: function(myString){  
+    stringArray = myString.split(/\W+/).filter(function(word) {
+      return word.length > 0;
+    })
+
+    capitalizedArray = [];
+
+    for (let i = 0; i < stringArray.length; i++) {
+      const capitalizedString = stringArray[i].charAt(0).toUpperCase() + stringArray[i].slice(1);
+      capitalizedArray.push(capitalizedString);
+    }
+    return capitalizedArray.join('_');
   },
 
-  compareArrays: function(){ 
-    // your code here (replace the return)
-    return "Finish compareArrays first!" 
+  compareArrays: function(firstArray, secondArray){ 
+    if (firstArray.length !== secondArray.length) {
+      return false;
+    }
+    for (let i = 0; i < firstArray.length; i++) {
+      if (firstArray[i] !== secondArray[i]) {
+        return false;
+      }
+    }
+    return true; 
   },
 
-  fizzBuzz: function(){  
-    // your code here
+  fizzBuzz: function(size){  
+    var fizzArray = [];
+    for (let i = 1; i <= size; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        fizzArray.push("FIZZBUZZ")
+      } else if (i % 3 === 0) {
+        fizzArray.push("FIZZ");
+      } else if (i % 5 === 0) {
+        fizzArray.push("BUZZ");
+      } else {
+        fizzArray.push(i);
+      }
+    }
+    return fizzArray;
   },
 
-  myMap: function(){  
+  myMap: function(myArray, myFunction) {  
     // your code here
+    var returnArray = [];
+    for (let i = 0; i < myArray.length; i++) {
+      returnArray.push(myFunction(myArray[i]));
+    }
+
+    return returnArray;
   },
 
-  primes: function(){  
+  primes: function(length){  
     // your code here
+    let primesArray = [2];
+
+    if (length < 2) {
+      return [];
+    } else if (length === 2) {
+      return primesArray;
+    }
+
+    for (let i = 3; i < length; i++) {
+      let isPrime = true;
+
+      // tests each i for primeness
+      for (let j = 2; j < i; j++) {
+        if (i % j === 0) {
+          isPrime = false;
+        }
+      }
+
+      if (isPrime){
+        primesArray.push(i);
+      }
+    }
+
+    return primesArray;
   },
 }
+
+// my test suite
+console.log(sprintFunctions.primes(8));
