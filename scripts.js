7d704ff0ae00425c71e82f9b87668b5a33ff066f
Aaron Garza <robertaarongarza@gmail.com>
2017-04-04 21:58:44 -0400

    First three exercises complete

--- scripts.js
+++ scripts.js
@@ -1,21 +1,37 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
-    // your code here
+  largestEl: function(myArray){  
+    var largest = 0;
+
+    for(var i = 0; i < myArray.length; i++) {
+      if(myArray[i] > myArray[largest]){
+        largest = i;
+      }
+    }
+    return myArray[largest];
   },
   
-  reversed: function(){  
-    // your code here
+  reversed: function(string){  
+    return string.split('').reverse('').join('');
   },
 
   loudSnakeCase: function(){  
     // your code here
   },
 
-  compareArrays: function(){ 
-    // your code here (replace the return)
-    return "Finish compareArrays first!" 
+  compareArrays: function(arr1, arr2){
+    if (arr1.length == arr2.length) {
+      for(var i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) 
+          return false;
+        };
+    } 
+    else {
+      return false;
+    }
+    return true;
+    // return "Finish compareArrays first!" 
   },
 
   fizzBuzz: function(){  
