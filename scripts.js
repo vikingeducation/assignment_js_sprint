d62709c2e3e0d533a405aec9dfaf272d213df077
Tyler Ketron <tketron@gmail.com>
2017-04-18 22:46:09 -0700

    complete 'snake case' function

--- scripts.js
+++ scripts.js
@@ -4,21 +4,31 @@ var sprintFunctions = {
   largestEl: function(inputArray){
     var largestElement = inputArray[0];
     for (i=1; i<inputArray.length+1; i++) {
-      console.log(inputArray[i]);
       if (inputArray[i] > largestElement) {
         largestElement = inputArray[i];
       };
     };
-    console.log(largestElement);
     return largestElement;
   },
 
-  reversed: function(){
-    // your code here
+  reversed: function(inputString){
+    var reversedString = "";
+    for (i=inputString.length-1; i>=0; i--) {
+      reversedString += inputString[i];
+    };
+    return reversedString;
   },
 
-  loudSnakeCase: function(){
-    // your code here
+  loudSnakeCase: function(inputString){
+    //iterate through string
+    var splitOutputString = [];
+    var splitInputString = inputString.split(/\W+/);
+    splitInputString.pop();
+    splitInputString.forEach(function(item) {
+      splitOutputString.push(item.charAt(0).toUpperCase() + item.substr(1));
+    });
+    //if character at that location is greater than digits, replace with underscore
+    return splitOutputString.join("_");
   },
 
   compareArrays: function(){
