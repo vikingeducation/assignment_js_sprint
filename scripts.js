c4bd790685c93236932a8b3df765115888dc3f5e
Tyler Ketron <tketron@gmail.com>
2017-04-18 20:16:42 -0700

    complete 'test largest element' function

--- scripts.js
+++ scripts.js
@@ -1,32 +1,40 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
-    // your code here
+  largestEl: function(inputArray){
+    var largestElement = inputArray[0];
+    for (i=1; i<inputArray.length+1; i++) {
+      console.log(inputArray[i]);
+      if (inputArray[i] > largestElement) {
+        largestElement = inputArray[i];
+      };
+    };
+    console.log(largestElement);
+    return largestElement;
   },
-  
-  reversed: function(){  
+
+  reversed: function(){
     // your code here
   },
 
-  loudSnakeCase: function(){  
+  loudSnakeCase: function(){
     // your code here
   },
 
-  compareArrays: function(){ 
+  compareArrays: function(){
     // your code here (replace the return)
-    return "Finish compareArrays first!" 
+    return "Finish compareArrays first!"
   },
 
-  fizzBuzz: function(){  
+  fizzBuzz: function(){
     // your code here
   },
 
-  myMap: function(){  
+  myMap: function(){
     // your code here
   },
 
-  primes: function(){  
+  primes: function(){
     // your code here
   },
 }
