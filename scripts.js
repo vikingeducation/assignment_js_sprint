27c645b60b210e27517519d9e0d43df56f2f5fb1
Karan Singh <kar.n@icloud.com>
2017-04-26 16:02:37 -0700

    halfway done

--- scripts.js
+++ scripts.js
@@ -1,16 +1,31 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
+  largestEl: function(array){  
     // your code here
+    var bigEl = 0;
+    for(var i = 0; i < array.length; i++){
+      if(array[i] > bigEl){
+        bigEl = array[i];
+      }
+    }
+    return bigEl;
   },
   
-  reversed: function(){  
+  reversed: function(string){  
     // your code here
+    return string.split("").reverse().join("");
   },
 
-  loudSnakeCase: function(){  
+  loudSnakeCase: function(str){  
     // your code here
+     var words = [];
+  var replaced = str.replace(/[\W]+/g, " ");
+  var split = replaced.trim().split(" ");
+    for(var i = 0; i < split.length; i++){
+     words.push(split[i][0].toUpperCase() + split[i].slice(1));
+    }
+  return words.join('_');
   },
 
   compareArrays: function(){ 
