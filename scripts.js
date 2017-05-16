49150acff1a2db21c1e7abd4bcb9beb2b2a4db61
ke2uke <ke2uke@virginmedia.com>
2017-05-16 20:09:40 +0100

    loudSnakeCase v1.0

--- scripts.js
+++ scripts.js
@@ -19,8 +19,31 @@ var sprintFunctions = {
     return temp;
   },
 
-  loudSnakeCase: function(){
-    // your code here
+  loudSnakeCase: function(s){
+    var s1 = s.replace(/[!.]/g, "");
+    var s2 = s1.replace(/ /g, "_");
+    var s3 = "";
+    var check = false;
+    for (var i = 0; i < s2.length; i++) {
+      if (s2[i] == "_") {
+        if (s2[i+1] == "_") {
+
+        }
+        else {
+          check = true;
+          s3 += s2[i];
+        }
+      }
+      else if (check) {
+        s3 += s2[i].toUpperCase();
+        check = false;
+      }
+      else {
+        s3 += s2[i];
+      }
+    }
+    console.log(s3);
+    return s3;
   },
 
   compareArrays: function(){
