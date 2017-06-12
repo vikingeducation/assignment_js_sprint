0631e97ab7068943189ce5618bc6345abb155a0e
Chinmay Lonkar <clonkified@gmail.com>
2017-06-11 18:08:46 -0700

    JS Functions Complete

--- scripts.js
+++ scripts.js
@@ -1,32 +1,82 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
-    // your code here
+  largestEl: function(ar){  
+    let max = ar[0];
+    for(const el of ar) {
+      if(el > max) {
+        max = el;
+      }
+    }
+    return max;
   },
   
-  reversed: function(){  
-    // your code here
+  reversed: function(str){  
+    let nStr = "";
+    for(let i = str.length; i > 0; i--) {
+      nStr += str.charAt(i-1);
+    }
+    return nStr;
   },
 
-  loudSnakeCase: function(){  
-    // your code here
+  loudSnakeCase: function(str){
+    str = str.replace(/[!.?]/g, "").replace(/\s\s/g, " ").replace(/\b[a-z]/g, function(s) {
+      return s.toUpperCase();
+    }).replace(/\s/g, "_");
+    return str;
   },
 
-  compareArrays: function(){ 
-    // your code here (replace the return)
-    return "Finish compareArrays first!" 
+  compareArrays: function(ar1, ar2){ 
+    if(ar1.length != ar2.length) {
+      return false;
+    }
+    for(let i = 0; i < ar1.length; i++) {
+      if(ar1[i] !== ar2[i]) {
+        return false;
+      }
+    }
+    return true;
   },
 
-  fizzBuzz: function(){  
-    // your code here
+  fizzBuzz: function(num){  
+    let ar = [];
+    for(let i = 1; i <= num; i++) {
+      if(i % 3 === 0 && i % 5 === 0) {
+        ar.push("FIZZBUZZ");
+      } else if (i % 5 === 0) {
+        ar.push("BUZZ");
+      } else if (i % 3 === 0) {
+        ar.push("FIZZ");
+      } else {
+        ar.push(i);
+      }
+    }
+    return ar;
   },
 
-  myMap: function(){  
-    // your code here
+  myMap: function(ar, f){  
+    let newAr = [];
+    for(let i of ar) {
+      newAr.push(f(i));
+    }
+    return newAr;
   },
 
-  primes: function(){  
-    // your code here
+  primes: function(num){  
+    let p = [];
+    let found = false;
+    for(let i = 2; i < num; i++) {
+      found = false;
+      for(let j = 2; j < i; j++) {
+        if(i % j === 0) {
+          found = true;
+          break;
+        }
+      }
+      if(!found) {
+        p.push(i);
+      }
+    }
+    return p;
   },
 }
