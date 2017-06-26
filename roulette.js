318f15f82043f9e68a57721884b10fb038ad6748
Will Timpson <will.timpson@gmail.com>
2017-06-26 16:11:00 -0400

    Roulette: All bets, ugly

--- roulette.js
+++ roulette.js
@@ -3,23 +3,79 @@
 function Roulette(startCash) {
     this.cashAvailable = sanitize(startCash);
 
-    this.spin = function(betAmount, betNumber) {
+    this.spin = function(betAmount, betEntry) {
         if ( !(sanitize(betAmount)) ) {
             console.log(`Sorry, $${betAmount} is not a valid bet amount.`);
         } else if (betAmount > this.cashAvailable) {
             console.log(`Sorry, $${betAmount} is more than you can bet`);
-        } else if ( isNaN(betNumber) || (betNumber < 1) || (betNumber > 36) ) {
-            console.log(`Sorry, ${betNumber} is not a valid bet (1-36)`);
         } else {
-            let roll = Math.floor(Math.random() * (37 - 1)) + 1;
-            if (roll === betNumber) {
-                let winnings = 35 * betAmount;
-                this.cashAvailable += winnings;
-                console.log(`Congratulations! You win $${winnings}`);
-            } else {
-                this.cashAvailable -= betAmount;
-                console.log(`Sorry! You spun ${roll}`);
+            let allNums = []
+            for (let i = 1; i < 37; i++) {
+                allNums.push(i);
             }
+            let winners = [];
+            let ratio = 0;
+            switch(betEntry) {
+                case 0:
+                    winners = [wheelSpin()];
+                    ratio = 35;
+                    break;
+                case '00':
+                    winners = [wheelSpin()];
+                    ratio = 35;
+                    break;
+                case 'Even':
+                    winners = allNums.filter(i => i % 2 === 0);
+                    ratio = 1;
+                    break;
+                case 'Odd':
+                    winners = allNums.filter(i => i % 2 === 1);
+                    ratio = 1;
+                    break;
+                case '1 to 18':
+                    winners = allNums.filter(i => i < 19);
+                    ratio = 1;
+                    break;
+                case '19 to 36':
+                    winners = allNums.filter(i => i > 18);
+                    ratio = 1;
+                    break;
+                case '1st 12':
+                    winners = allNums.filter(i => i < 13);
+                    ratio = 2;
+                    break;
+                case '2nd 12':
+                    winners = allNums.filter(i => (i > 12) && (i < 25));
+                    ratio = 2;
+                    break;
+                case '3rd 12':
+                    winners = allNums.filter(i => i > 24);
+                    ratio = 2;
+                    break;
+                default:
+                    if (isNaN(betEntry) || (betEntry < 1) || (betEntry > 36)) {
+                        console.log(`Sorry, ${betEntry} is not a valid bet`);
+                    } else {
+                        winners = [betEntry];
+                        ratio = 35;
+                    };
+            }
+            if (ratio) {
+                let actualSpin = wheelSpin();
+                if (winners.indexOf(actualSpin) === -1){
+                    this.cashAvailable -= betAmount;
+                    console.log(`Sorry! You spun ${actualSpin}`)
+                } else {
+                    if ((betEntry === 0) || (betEntry === '00')) {
+                        actualSpin = betEntry
+                    }
+                    let winnings = betAmount * ratio;
+                    this.cashAvailable += winnings;
+                    console.log(`Congratulations! You rolled ${actualSpin} and win $${winnings}`);
+                }
+            }
+
+
         }
         this.bankroll();
     };
@@ -33,6 +89,11 @@ function Roulette(startCash) {
         this.bankroll();
     }
 };
+
+// Generate a random wheel spin
+var wheelSpin = function() {
+    return Math.floor(Math.random() * (37 - 1)) + 1;
+}
 
 // Ensure that the cash given is a number
 var sanitize = function(inputCash) {
