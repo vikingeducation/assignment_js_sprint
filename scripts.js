b61117edf65384e546a82f42ff3c8c170c8ee14c
Jeff Bernstein <jeffreybbernstein@gmail.com>
2017-06-26 21:45:43 -0700

    still playing with loudSnakeCase

--- scripts.js
+++ scripts.js
@@ -1,32 +1,64 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
-    // your code here
-  },
-  
-  reversed: function(){  
-    // your code here
+
+
+
+  // largestEl takes an array and returns the largest element.
+  largestEl: function(userArray){
+
+    var largestValue = userArray[0];
+    for (var i = 0; i < userArray.length; i++) {
+      if (userArray[i] > largestValue) {
+        largestValue = userArray[i];
+      }
+    }
+
+    return largestValue;
+
   },
 
-  loudSnakeCase: function(){  
-    // your code here
+  // reversed takes a string and reverses it.
+  reversed: function(string){
+
+    var revString = "";
+    for (var i = string.length - 1; i > -1; i--) {
+      revString += string[i];
+    }
+
+    return revString;
+
   },
 
-  compareArrays: function(){ 
+// loudSnakeCase takes a full sentence and puts it into "Loud_Snake_Case" format but strips out any non-character elements (like punctuation).
+  loudSnakeCase: function(sentence){
+
+    var snakeSentence = sentence.toLowerCase();
+    snakeSentence = snakeSentence.replace("[^a-zA-Z\\s]", "");
+    snakeSentence = snakeSentence.split(" ");
+
+    for (var i = 0; i < snakeSentence.length; i++) {
+      snakeSentence[i] = snakeSentence[i].charAt(0).toUpperCase() + snakeSentence[i].slice(1);
+    }
+
+    return snakeSentence.join('_');
+
+  },
+
+  compareArrays: function(){
     // your code here (replace the return)
-    return "Finish compareArrays first!" 
+    return "Finish compareArrays first!"
   },
 
-  fizzBuzz: function(){  
+  fizzBuzz: function(){
     // your code here
   },
 
-  myMap: function(){  
+  myMap: function(){
     // your code here
   },
 
-  primes: function(){  
+  primes: function(){
     // your code here
   },
 }
