d97e6086ae45d5dbabda464c258091f224c01d30
ruby -v <oblacishere@gmail.com>
2017-06-30 04:58:40 +0100

    indention corrected

--- Roulette.js
+++ Roulette.js
@@ -2,37 +2,37 @@
 
 function Roulette(bankroll) {
 	this.bankroll = bankroll;
-		this.buyIn = function(amount) {
-			console.log("You bought in " + amount);
-				this.bankroll += amount;
-					console.log("You now have " + this.bankroll);
+	this.buyIn = function(amount) {
+		console.log("You bought in " + amount);
+		this.bankroll += amount;
+		console.log("You now have " + this.bankroll);
+	}
+
+	this.spin = function(amount, number) {
+		this.amount = amount;
+		if(this.amount > this.bankroll) {
+			alert('INSUFFICIENT FUNDS');
+			return this.bankroll;
 		}
 
-		this.spin = function(amount, number) {
-			this.amount = amount;
-				if(this.amount > this.bankroll) {
-						alert('INSUFFICIENT FUNDS');
-							return this.bankroll;
-				}
+		this.bankroll -= this.amount;
+		this.number = number;
+		if(this.number > 36){
+			alert('Numbers must be between 1-36');
+			return this.number;
+		}
 
-				this.bankroll -= this.amount;
-					this.number = number;
-						if(this.number > 36){
-							alert('Numbers must be between 1-36');
-								return this.number;
-						}
-
-			var bonus = 35;
-				var spinResult = Math.floor(Math.random() * ((36-1)+1) + 1);
- 					if(spinResult == this.number) {
-						this.amount *= bonus;
-							this.bankroll += this.amount;
-								console.log('you win ' + this.amount + " the spin was " + spinResult);
-									console.log("You now have " + this.bankroll);
-					} else {
-						console.log('you lose,the spin was ' + spinResult);
-							console.log("You now have " + this.bankroll);
-					}
+		var bonus = 35;
+		var spinResult = Math.floor(Math.random() * ((36-1)+1) + 1);
+ 		if(spinResult == this.number) {
+			this.amount *= bonus;
+			this.bankroll += this.amount;
+			console.log('you win ' + this.amount + " the spin was " + spinResult);
+			console.log("You now have " + this.bankroll);
+		} else {
+			console.log('you lose,the spin was ' + spinResult);
+			console.log("You now have " + this.bankroll);
+		}
     }
 }
 
