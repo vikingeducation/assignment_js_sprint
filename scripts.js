7b92baaa8da9bb2ff4442aff1a2fddd7091366df
Thomas Hauge <thomas.sommerfelt.hauge@gmail.com>
2017-08-15 17:49:01 +0200

    finishing basic problems

--- scripts.js
+++ scripts.js
@@ -1,32 +1,87 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
-    // your code here
-  },
-  
-  reversed: function(){  
-    // your code here
-  },
-
-  loudSnakeCase: function(){  
-    // your code here
+  largestEl: function(array){
+    var largest = 0;
+    for (var i=0; i < array.length; i++) {
+      if (array[i] > largest) {
+        largest = array[i];
+      }
+    }
+    return largest;
   },
 
-  compareArrays: function(){ 
-    // your code here (replace the return)
-    return "Finish compareArrays first!" 
+  reversed: function(string){
+    var newArray = [];
+    for (var i=0; i < string.length; i++) {
+      newArray.unshift(string[i]);
+    }
+    return newArray.join("");
   },
 
-  fizzBuzz: function(){  
-    // your code here
+  loudSnakeCase: function(sentence){
+    var punctuation = /\W/g;
+    sentence = sentence.toLowerCase().replace(/ /g, "_").replace(punctuation, "").replace(/_(?=_)/g, "");
+    sentence = sentence.split("_");
+    for (var i=0; i < sentence.length; i++) {
+      sentence[i] = sentence[i].charAt(0).toUpperCase() + sentence[i].slice(1);
+    }
+    return sentence.join("_");
   },
 
-  myMap: function(){  
-    // your code here
+  compareArrays: function(arr1, arr2){
+    equal = true;
+    for (var i=0; i<arr1.length; i++) {
+      if (arr1[i] != arr2[i]) {
+        equal = false;
+      }
+    }
+    return equal;
   },
 
-  primes: function(){  
-    // your code here
+  fizzBuzz: function(number){
+    var outputArr = [];
+    for (var i=1; i <= number; i++) {
+      if ((i % 3 === 0) && (i % 5 === 0)) {
+        outputArr.push("FIZZBUZZ");
+      } else if (i % 3 === 0) {
+        outputArr.push("FIZZ");
+      } else if (i % 5 === 0) {
+        outputArr.push("BUZZ");
+      } else {
+        outputArr.push(i);
+      }
+    }
+    return outputArr;
+  },
+
+  myMap: function(arr, func){
+    var returnArr = [];
+    for (var i = 0; i < arr.length; i++) {
+      returnArr.push(func(arr[i]));
+    }
+    return returnArr;
+  },
+
+  primes: function(number){
+    // https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes#Algorithm_and_variants
+    var integers = [undefined, undefined];
+    for (var i = 2; i < number; i++) {
+      integers[i] = true;
+    }
+    for (var i = 2; i < (Math.sqrt(number)); i++) {
+      if (integers[i]) {
+        for (var j = i*i; j < number; j += i) {
+          integers[j] = false;
+        }
+      }
+    }
+    primeArr = [];
+    for (var k = 2; k < number; k++) {
+      if (integers[k]) {
+        primeArr.push(k);
+      }
+    }
+    return primeArr;
   },
 }
