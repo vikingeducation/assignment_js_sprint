69aa03daeacd30767fadb11004c6fa8975f3c3bf
Visiona <darbis@gmail.com>
2017-08-17 19:43:53 +0100

    Created functions myMap, primes

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
 // FILL IN THE FUNCTIONS BELOW
-// largestEl takes an array and returns the largest element.
+
 var sprintFunctions = {
+
+// largestEl takes an array and returns the largest element.
   largestEl: function(array){
     return array.sort(function(a,b) { return a-b; }).slice(-1)[0]
   },
 
+// reversed takes a string and reverses it.
   reversed: function(string){
     var newString = "";
     for(var i = string.length - 1; i >= 0; i--) {
@@ -13,6 +16,7 @@ var sprintFunctions = {
     return newString;
   },
 
+// loudSnakeCase takes a full sentence and puts it into "Loud_Snake_Case" format but strips out any non-character elements (like punctuation).
   loudSnakeCase: function(string){
     array = string.replace(/[^\w\s]/gi, "");
     array = array.split(/[^\w\S]/);
@@ -26,6 +30,7 @@ var sprintFunctions = {
     return new_array.join(",").replace(/,/g, "_");
   },
 
+// compareArrays takes two arrays and checks to see if they are equal (same contents in the same order). Assume they're not nested.
   compareArrays: function(array1, array2){
     check = false;
     if (array1.length === array2.length) {
@@ -41,6 +46,7 @@ var sprintFunctions = {
     }
   },
 
+// fizzBuzz takes an input of a number and returns an array containing all the elements from 1 to that number. Each element divisible by 3 is replaced by "FIZZ", each element divisible by 5 is replaced by "BUZZ" and each element divisible by both 3 and 5 is replaced by "FIZZBUZZ".
   fizzBuzz: function(number){
     array = [];
     for (var i=1; i <= number; i++) {
@@ -57,11 +63,22 @@ var sprintFunctions = {
     return array;
   },
 
-  myMap: function(){
-    // your code here
+// myMap takes an array and a function. It passes every element from the array into that function, in turn, running the function.
+  myMap: function(array, function_name){
+    for(i=0; i<array.length; i++) {
+      array[i] = function_name(array[i]);
+    };
+    return array;
   },
 
-  primes: function(){
-    // your code here
+// primes takes a number and outputs an array containing all prime numbers that occur prior to that number
+  primes: function(number){
+    array = [];
+    for (var i=2; i < number; i++) {
+      if ( (i === 2 || i === 3) || (i%2 !== 0 && i%3 !== 0) ) {
+        array.push(i);
+      };
+    };
+    return array;
   },
 }
