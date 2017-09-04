6ea98bb6bb20bde40bc1843abdddf056db9044a7
Ian Halverson <halversoncode@gmail.com>
2017-09-04 10:01:36 -0400

    rename

--- scripts.js
+++ scripts.js
@@ -4,8 +4,8 @@ var sprintFunctions = {
 
   largestEl: function(array){
     var largest = array[0];
-    for(i = 0; i < array.length; i += 1) {
-      if(array[i] > largest) {
+    for (i = 1; i < array.length; i++) {
+      if (array[i] > largest) {
         largest = array[i];
       }
     }
@@ -13,102 +13,110 @@ var sprintFunctions = {
   },
 
   reversed: function(string){
-    var newString = "";
-    for (i = (string.length - 1); i >= 0; i -= 1) {
-      newString += string[i];
-    }
+    var newString;
+    newString = string.split("");
+    newString = newString.reverse();
+    newString = newString.join("");
     return newString;
   },
 
   loudSnakeCase: function(string){
-    var newString = "";
+    var newString = [];
     var alphaNum = /^[a-z0-9]+$/i;
-    if(alphaNum.test(string[0])) {
-       newString += string[0].toUpperCase();
-     }
-     else if (string[0] === " ") {
-       newString += "_"
-     }
-     else { newString += ""}
-    for (i = 1; i < string.length; i += 1) {
-      switch (string[1] === string[1]) {
-        case (alphaNum.test(string[i]) === false && string[i] !== " "):
-      newString += "";
-      break;
-      case (alphaNum.test(string[i]) === true && (alphaNum.test(string[i-1]) !== true || (string[i-1] === " ") === true)):
-        newString += string[i].toUpperCase();
-      break;
-  case (alphaNum.test(string[i]) === true && string[i-1] !== " "):
-          newString += string[i];
-      break;
-case (string[i] === " " && string[i+1] === " "):
-         newString += "";
-      break;
-      case (string[i] === " "):
-          newString += "_";
-      break;
+    if (string[0] === " ") {
+      newString[0] = "_";
     }
+    else if (alphaNum.test(string[0])) {
+      newString[0] = string[0].toUpperCase();
     }
-  return newString;
+    else {
+      newString[0] = "";
+    }
+    for (i = 1; i < string.length; i++) {
+      if (string[i] === " ") {
+        if (string[i-1] === " ") {
+          newString.push("");
+        }
+        else {
+          newString.push("_");
+        }
+      }
+      else if (alphaNum.test(string[i])) {
+        if (string[i - 1] === " " || alphaNum.test(string[i-1]) === false) {
+          newString.push(string[i].toUpperCase());
+        }
+        else {
+          newString.push(string[i]);
+        }
+      }
+      else {
+        newString.push("");
+      }
+    }
+    return newString.join("");
 },
 
   compareArrays: function(array1, array2){
-    var arrayLength = 0;
-         if(array1.length === array2.length) {
-  for(i = 0; i < array1.length; i += 1) {
-        if (array1[i] === array2[i]) {
-          arrayLength += 1;
-        }
+    var matches = [];
+    if (array1.length !== array2.length) {
+      return false;
+    }
+    else {
+    for (i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+        matches.push(1);
       }
     }
-    if (array1.length === arrayLength) {
-      return true
-    }
-    else {return false
-         }
+  }
+  if (matches.length === array1.length) {
+    return true;
+  }
+  else {
+    return false;
+  }
   },
 
   fizzBuzz: function(end){
-    var FIZZBUZZ = [];
-    for(i = 1; i <= end; i++) {
+    var fizzBuzzArray = [];
+    for (i = 1; i <= end; i++) {
       if (i % 15 === 0) {
-        FIZZBUZZ.push("FIZZBUZZ");
+        fizzBuzzArray.push("FIZZBUZZ");
       }
       else if (i % 5 === 0) {
-        FIZZBUZZ.push("BUZZ");
+        fizzBuzzArray.push("BUZZ");
       }
       else if (i % 3 === 0) {
-        FIZZBUZZ.push("FIZZ");
+        fizzBuzzArray.push("FIZZ");
       }
-      else FIZZBUZZ.push(i);
+      else {
+        fizzBuzzArray.push(i);
+      }
     }
-    return FIZZBUZZ;
+    return fizzBuzzArray;
   },
 
   myMap: function(array1, funct1) {
-    var returnArray = [];
-    for (i = 0; i < array1.length; i++) {
-      returnArray.push(funct1(array1[i]));
+    var answersArray = [];
+    for (i = 0; i < array1.length; i++){
+      answersArray.push(funct1(array1[i]));
     }
-    return returnArray;
+    return answersArray;
   },
 
   primes: function(end){
-    var primesArr = [];
-    var checkPrime = function(input){
-      var outputArr = [];
-        for(j = 1; j <= input; j++) {
-          if(input % j === 0) {
-            outputArr.push(input);
-          }
+    var primes = [];
+    var divisors = [];
+    for (i = 2; i < end; i++) {
+      for (j = 2; j <= i; j++) {
+        if (i % j === 0) {
+          divisors.push("divisor");
         }
-      return outputArr.length;
-      };
-      for(i = 1; i <= end; i++) {
-        if (checkPrime(i) === 2) {
-        primesArr.push(i);
       }
+      if (divisors.length < 2) {
+        primes.push(i);
+      }
+      divisors = [];
     }
-    return primesArr;
+    return primes;
   },
 }
