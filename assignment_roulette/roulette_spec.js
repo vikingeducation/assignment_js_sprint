ae96a8a16f8a610630d43ab51476077c77c022e5
Davey Wright <davey.w.wright@gmail.com>
2017-10-17 07:38:03 -0700

    complete roulette game

---
+++ assignment_roulette/roulette_spec.js
@@ -0,0 +1,11 @@
+var tester = {
+	init: function(rouletteObj){
+		this.rouletteObj = rouletteObj;
+	},
+
+	testPlaceBet: function(){
+
+	}
+}
+
+$(document).ready(function(){tester.init( roulette )})
