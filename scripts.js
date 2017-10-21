938277f3bd7916f39499677f257e2b3f7665800c
Simon <Simon_Freelance@hotmail.com>
2017-10-21 13:07:02 -0400

    Finished every function except myMap

--- scripts.js
+++ scripts.js
@@ -20,9 +20,12 @@ var sprintFunctions = {
   },
 
   loudSnakeCase: function(str){
-    let newStr = str.replace("/\w/g","s");
-
-    return newStr;
+    let newStr = str.split(" ").filter(Boolean);
+    for(let i = 0; i<newStr.length; i++) {
+      newStr[i] = newStr[i].replace(/\W/g,"");
+      newStr[i] = newStr[i][0].toUpperCase() + newStr[i].slice(1);
+    }
+    return newStr.join("_");
   },
 
   compareArrays: function(arr,comparedArr){
@@ -32,25 +35,40 @@ var sprintFunctions = {
   fizzBuzz: function(n){
     const arr = [];
     for(let i =1; i<=n; i++) {
-      i % 3 === 0 && i % 5 === 0 ? arr.push("FizzBuzz"):
-      i % 3 === 0 ? arr.push("Fizz"):
-      i % 5 === 0 ? arr.push("Buzz"):arr.push(i);
+      i % 3 === 0 && i % 5 === 0 ? arr.push("FIZZBUZZ"):
+      i % 3 === 0 ? arr.push("FIZZ"):
+      i % 5 === 0 ? arr.push("BUZZ"):arr.push(i);
     }
     return arr;
   },
 
-  myMap: function(){
-    // your code here
+  myMap: function(arr,func){
   },
 
   primes: function(n){
+    
+    let sqrtNum;
+    const storePrimes = [];
+    if(n===2) {return [2]}
+    if(n>2) {storePrimes.push(2,3)}
 
+    for(let i =1; i<n; i++) {
+      sqrtNum = Math.sqrt(i);
+    if(Number.isInteger(sqrtNum) === true || i % 2 === 0 || i % 3 ===0) {
+      continue;
+      }
+    storePrimes.push(i);
+  }
 
+return storePrimes;
   },
 }
 
-console.log(sprintFunctions.largestEl(["dragon","bunny","dog","monkey"]));
+console.log(sprintFunctions.largestEl(["dragon","bunny","dodragog","monkey"]));
 console.log(sprintFunctions.reversed("dragon"));
-console.log(sprintFunctions.loudSnakeCase("how is . the weather"));
+console.log(sprintFunctions.loudSnakeCase("how is   .th%&e w#!eather"));
 console.log(sprintFunctions.compareArrays([1,2,3],[1,2,3]));
 console.log(sprintFunctions.fizzBuzz(15));
+console.log(sprintFunctions.myMap([1,2,3,4,5]));
+console.log(sprintFunctions.primes(25));
+console.log(sprintFunctions.myMap([5,7,3,2]));
