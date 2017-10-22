72d47eb7a8b922eae4bff60c43abc9ba051d8ba5
Visiona <darbis@gmail.com>
2017-10-22 23:12:02 +0100

    Updated README

--- roulette_game.js
+++ roulette_game.js
@@ -50,3 +50,10 @@ function Roulette(init_bankroll) {
      console.log("You now have $" + deposit);
    };
  };
+
+ var http = require('http');
+    http.createServer(function (req, res) {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end(Roulette(init_bankroll));
+    }).listen(1337, "127.0.0.1");
+    console.log('Server running at http://127.0.0.1:1337/');
