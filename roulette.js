b2114b53bff14730450e039d73e4d8accce782a5
tzvi <tzvika200@yahoo.com>
2017-11-23 15:10:52 -0500

    added interface

--- roulette.js
+++ roulette.js
@@ -1,19 +1,35 @@
 let index = 1,
   players = {},
   player = {
-    log: function() {
-      return this
+    betAmount: 0,
+    betNumber: 1,
+    betType: "",
+    buyInAmount: 100,
+    setBetAmount:function(betAmount){
+      this.betAmount = parseInt(betAmount)
     },
-    cash: 100
-  };
-player.__proto__.buyIn = function(amount) {
-  this.cash += amount
+    setBetNumber:function(betNumber){
+      this.betNumber = parseInt(betNumber)
+    },
+    setBetType:function(betType){
+      this.betType = parseInt(betType)
+    },
+    setBuyInAmount:function(buyInAmount){
+      this.cash += parseInt(buyInAmount)
+    },
+    cash: 0
+  }
+
+player.__proto__.buyIn = function() {
+  this.cash += this.buyInAmount
   this.showCash()
 }
-player.__proto__.roll = function(number, bet, type = "none") {
+player.__proto__.roll = function() {
   var rolled = Math.round(Math.random() * 36)
+  player.rolled = rolled
   var match = ""
-  if (number === "none") {
+  let type = document.getElementById("betType").value
+  if (this.betNumber === "") {
     switch (type) {
       case "even":
         if (rolled % 2 === 0) {
@@ -51,20 +67,22 @@ player.__proto__.roll = function(number, bet, type = "none") {
         }
         break;
     }
-    console.log(match, number, rolled)
+    console.log(match, this.betNumber, rolled)
   } else {
-    number <= 36 ? console.log(match, number, rolled) : console.log(`${number} too large`)
-    match = number === rolled
+    this.betNumber <= 36 ? console.log(match, this.betNumber, rolled) : console.log(`${this.betNumber} too large`)
+    match = this.betNumber === rolled
   }
   if(match){
-    this.cash += bet
-    console.log( `you won ${bet} ${this.cash} is your new total` )
+    this.cash += this.betAmount
+    console.log( `you won ${this.betAmount}, ${this.cash} is your new total` )
   } else {
-    this.cash -= bet;
-    console.log( `you lost ${bet} ${this.cash} is your new total` );
+    this.cash -= this.betAmount;
+    console.log( `you lost ${this.betAmount}, ${this.cash} is your new total` );
   }
 }
+
 player.__proto__.showCash = function() {
+  console.log(this)
   console.log(this.cash)
 }
 while (index < 3) {
@@ -75,3 +93,46 @@ let {
   player1,
   player2
 } = players
+
+
+let currentPlayer = player1
+let currentIdmoney = "player1money"
+let currentIdbet = "player1lastbet" 
+let currentIdresult = "player1lastresult"
+
+function changePlayer(){
+  if(currentPlayer === player1){
+    currentPlayer = player2
+    currentIdmoney = "player2money"
+    currentIdbet = "player2lastbet" 
+    currentIdresult = "player2lastresult"
+  } else  {
+    currentPlayer = player1
+    currentIdmoney = "player1money"
+    currentIdbet = "player1lastbet" 
+    currentIdresult = "player1lastresult" 
+  } 
+}
+
+$("#setAll").click(function(){
+    currentPlayer.setBetAmount(document.getElementById("betAmount").value)
+    currentPlayer.setBetNumber(document.getElementById("betNumber").value)
+    currentPlayer.setBetType( document.getElementById("betType").value)
+    currentPlayer.setBuyInAmount(document.getElementById("buyInAmount").value)
+})
+    
+$("#buyin").click(function(){
+  currentPlayer.buyIn()
+})
+$("#roll").click(function(){
+  currentPlayer.roll()  
+  $(`#${currentIdmoney}`).html(currentPlayer.cash)
+  $(`#${currentIdbet}`).html(currentPlayer.betNumber)
+  $(`#${currentIdresult}`).html(currentPlayer.rolled)
+  changePlayer()
+
+})
+$("#showcash").click(function(){
+  currentPlayer.showCash()
+})
+
