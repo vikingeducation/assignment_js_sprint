2e31ac9464ebf17ce118983444812fd816f8d334
Brennan <31666354+BrennanFulmer@users.noreply.github.com>
2017-11-27 17:32:34 -0500

    now with barebones version of roulette game

--- roulette.js
+++ roulette.js
@@ -22,3 +22,34 @@ r.buyIn( 1000 )
 "00" (35:1), "Even" (1:1), "Odd" (1:1), "1 to 18" (1:1), "19 to 36" (1:1),
 "1st 12" (2:1), "2nd 12" (2:1), or "3rd 12" (2:1).
 */
+
+var bankroll = 100;
+
+var betSize = Number(prompt("Please enter how much you'd like to bet, current bankroll is $" + bankroll));
+console.log("You bet $" + betSize);
+
+var betTarget = Number(prompt("Now enter what number you'd like to bet on between 1 and 36"));
+console.log("The bet was on " + betTarget);
+
+function spinning(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var spinResult = spinning(1, 36);
+console.log("The spin result was " + spinResult);
+
+if (betTarget === spinResult) {
+  bankroll = (bankroll - betSize) + (betSize * 35);
+  console.log("You won!");
+  } else {
+    bankroll = bankroll - betSize
+    console.log("You lost");
+  };
+console.log("Your bankroll is now $" + bankroll);
+
+var buyIn = Number(prompt("If you'd like to increase your bankroll enter how much by now, enter 0 otherwise"));
+bankroll = bankroll + buyIn;
+console.log("You bought in $" + buyIn);
+console.log("So your bankroll is now $" + bankroll)
