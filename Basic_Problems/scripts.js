2e3a000e2c5d140d185acc0650a957679a514cfe
Brennan <31666354+BrennanFulmer@users.noreply.github.com>
2017-11-27 21:54:24 -0500

    improved scripts.js with more conventional formatting and again rewrote Loud_Snake_Case as previous version added useless code to improve read-ability but now that I know how much space can be added its not necessary

--- Basic_Problems/scripts.js
+++ Basic_Problems/scripts.js
@@ -1,77 +1,80 @@
-
 var sprintFunctions = {
-
-// takes an array and returns the largest element.
+  // takes an array and returns the largest element.
   largestEl: function(array) {
-      var highest = 0;
-      var counter = 0;
+    var highest = 0,
+      counter = 0;
     while (counter < array.length) {
       if (highest < array[counter]) {
         highest = array[counter];
       }
-      counter += 1
+      counter += 1;
     }
-      return highest;
+    return highest;
   },
 
-//takes a string and reverses it.
+  //takes a string and reverses it.
   reversed: function(string) {
-    return string.split("").reverse().join("");
+    return string
+      .split("")
+      .reverse()
+      .join("");
   },
 
-/*takes a full sentence and puts it into "Loud_Snake_Case" format but strips
+  /*takes a full sentence and puts it into "Loud_Snake_Case" format but strips
 out any non-character elements (like punctuation).*/
   loudSnakeCase: function(sentence) {
-    var lowered = sentence.toLowerCase();
-    var onlySpacesAndLetters = lowered.replace(/[^A-Za-z ]/g,"");
-    var singleSpace = onlySpacesAndLetters.replace(/ +/g," ");
-    var capFirstLetter = singleSpace.replace(/\b./g, function(firstletter){ return firstletter.toUpperCase(); });
-    var spaceToUnderscore = capFirstLetter.replace(/ /g,"_");
-    return spaceToUnderscore;
+    return sentence
+      .toLowerCase()
+      .replace(/[^A-Za-z ]/g, "")
+      .replace(/ +/g, " ")
+      .replace(/\b./g, function(firstletter) {
+        return firstletter.toUpperCase();
+      })
+      .replace(/ /g, "_");
   },
 
-/*takes two arrays and checks to see if they are equal (same contents in the
+  /*takes two arrays and checks to see if they are equal (same contents in the
 same order). Assume they're not nested.*/
-  compareArrays: function(array1,array2) {
-    return array1.join(',') === array2.join(',');
-},
+  compareArrays: function(array1, array2) {
+    return array1.join(",") === array2.join(",");
+  },
 
-/*takes an input of a number and returns an array containing all the elements
+  /*takes an input of a number and returns an array containing all the elements
 from 1 to that number. Each element divisible by 3 is replaced by "FIZZ", each
 element divisible by 5 is replaced by "BUZZ" and each element divisible by
 both 3 and 5 is replaced by "FIZZBUZZ". Eg. fizzBuzz(6) => [ 1, 2, "FIZZ", 4,
 "BUZZ", "FIZZ" ]*/
   fizzBuzz: function(number) {
-      var count = 1;
-      var patternoutput = [];
+    var count = 1,
+      patternoutput = [];
     while (count <= number) {
       if (count % 3 === 0 && count % 5 === 0) {
-        patternoutput.push('FIZZBUZZ');
+        patternoutput.push("FIZZBUZZ");
       } else if (count % 3 === 0) {
-          patternoutput.push('FIZZ');
-        } else if (count % 5 === 0) {
-            patternoutput.push('BUZZ');
-          } else {
-            patternoutput.push(count);
-            }
+        patternoutput.push("FIZZ");
+      } else if (count % 5 === 0) {
+        patternoutput.push("BUZZ");
+      } else {
+        patternoutput.push(count);
+      }
       count += 1;
     }
-      return patternoutput;
+    return patternoutput;
   },
 
-/*takes an array and a function. It passes every element from the array into
+  /*takes an array and a function. It passes every element from the array into
 that function, in turn, running the function. The returned array should be
 filled with each of the return statements from that function.*/
   myMap: function(AnArray, aFunction) {
     return AnArray.map(aFunction);
   },
 
-/*takes a number and outputs an array containing all prime numbers that occur
+  /*takes a number and outputs an array containing all prime numbers that occur
 prior to that number, e.g. primes(8) => [2,3,5,7]*/
   primes: function(aNumber) {
-      var options = [];
     if (aNumber > 2) {
-        var increment = 2;
+      var options = [],
+        increment = 2;
       while (increment < aNumber) {
         if (increment % 2 !== 0 || increment === 2) {
           options.push(increment);
@@ -79,21 +82,21 @@ prior to that number, e.g. primes(8) => [2,3,5,7]*/
         increment += 1;
       }
       function primeFind(integer) {
-          var check = integer - 1;
-          var primey = true;
+        var check = integer - 1,
+          primey = true;
         while (check > 1) {
           if (integer % check === 0) {
-            primey = false
+            primey = false;
             break;
           }
           check -= 1;
         }
-        return primey
+        return primey;
       }
-        var primes = options.filter(primeFind);
-          return primes;
+      var primes = options.filter(primeFind);
+      return primes;
     } else {
       return options;
     }
-  },
-}
+  }
+};
