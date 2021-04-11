3919c2063bfd9a5bbc748425f2b6001e67f7a4ec
Gjonçari Jurgen - AlbaNSI <j.gjoncari@albansi.com>
2021-04-11 22:30:28 +0200

    Initial commit

--- scripts.js
+++ scripts.js
@@ -1,16 +1,56 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(){  
-    // your code here
+  largestEl: function(array){  
+    array.sort();
+
+    let last = array.length - 1;
+    return array[last];
   },
   
-  reversed: function(){  
-    // your code here
+  reversed: function(string){  
+    let array = new Array;
+    for (let i = 0; i < string.length; i++) {
+      array.unshift(string[i]);
+    }
+    
+    return array.join("");
   },
 
-  loudSnakeCase: function(){  
-    // your code here
+  loudSnakeCase: function(string){
+    // turn string into an array
+    let array = string.split(" ");
+
+    // remove empty elements from the array
+    let nonEmpty = array.filter(Boolean);
+
+    // for every word in the array, keep only alphanumeric
+    nonEmpty.forEach(word => {
+      let charArray = word.split("");
+
+      charArray.filter(function(char) {
+        if (
+          !(char in "abcdefghijklmnopqrstuvxywz".split("")) &&
+          !(char in "ABCDEFGHIJKLMNOPQRSTUVXYWZ".split("")) &&
+          !(char in "0123456789")) {
+            char.replace(char, "");
+          }
+      )
+      }
+      };
+    );
+    
+    let isalnum = function(char) {
+    }
+
+    // write every word in loud case
+    let makeLoud = function(word) {
+      let alnumWord = word.filter(isalnum(c));
+      return alnumWord[0].toUpperCase() + alnumWord.slice(1, alnumWord.length);
+    }
+    
+    // return the array as a string joined by underscore 
+    return loud.join("_");
   },
 
   compareArrays: function(){ 
